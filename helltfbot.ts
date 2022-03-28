eeaa5245678e9745c3585e3892b21752914a0005
helltf <Dario.Zachow5530@gmail.com>
2022-03-28 23:32:15 +0200

    some testing

--- helltfbot.ts
+++ helltfbot.ts
@@ -1,1 +1,7 @@
-console.log("hello")
+import 'dotenv/config'
+import { getMainClientConfig } from './config/config.js'
+import * as tmi from 'tmi.js'
+
+let config = getMainClientConfig()
+
+console.log(config)
