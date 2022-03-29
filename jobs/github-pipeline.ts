87a75e30ebd517b760ee097da9679c023fe46462
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 19:39:04 +0200

    fixed setting of count

--- jobs/github-pipeline.ts
+++ jobs/github-pipeline.ts
@@ -1,20 +1,37 @@
 import { hb } from './../helltfbot.js'
 import { getPipeLineData, PipelineData } from '../api/github/github-api.js'
 import { Projects } from '../api/github/github-projects.js'
-let count: number = undefined
+
+let counts = new Map<Projects, number>([
+	[Projects.helltfbot_v2, undefined],
+	[Projects.go_discord_bot, undefined],
+	[Projects.inst_mono, undefined],
+])
+
+const setCount = (project: Projects, value: number) => {
+	counts.set(project, value)
+}
 
 const updateGithubPipeline = async () => {
-	let { success, data, error } = await getPipeLineData(Projects.helltfbot_v2)
+	for (let [project, count] of counts) {
+		checkForUpdate(project, count)
+	}
+}
+
+const checkForUpdate = async (project: Projects, count: number) => {
+
+	let { success, data, error } = await getPipeLineData(project)
 
 	if (!success) return
 
 	if (count === undefined) {
-		count = data.count
+		setCount(project, data.count)
+		return
 	}
 
 	if (count !== data.count && data.status === 'completed') {
 		announceNewPipeLine(data)
-		count = data.count
+		setCount(project, data.count)
 	}
 }
 
