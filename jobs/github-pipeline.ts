f6b5ece40e2846d557fbdf8a0e4473aac8a7a812
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 18:52:06 +0200

    send twitch message

--- jobs/github-pipeline.ts
+++ jobs/github-pipeline.ts
@@ -1,9 +1,9 @@
+import { hb } from './../helltfbot.js'
 import { getPipeLineData, PipelineData } from '../api/github/github-api.js'
 import { Projects } from '../api/github/github-projects.js'
 let count: number = undefined
 
 const getData = async () => {
-
 	let { success, data, error } = await getPipeLineData(Projects.helltfbot_v2)
 
 	if (!success) return
@@ -13,10 +13,9 @@ const getData = async () => {
 	}
 
 	if (count !== data.count) {
-		announceNewPipeLsine(data)
-        count = data.count
+		announceNewPipeLine(data)
+		count = data.count
 	}
-    
 }
 
 function announceNewPipeLine({
@@ -26,11 +25,15 @@ function announceNewPipeLine({
 	branch,
 }: PipelineData) {
 	if (status === 'completed' && conclusion === 'success') {
-		console.log(
+		hb.client.say(
+			'helltf',
 			`catJAM 👉 pipeline in ${repository} on branch ${branch} was successful`
 		)
-	} else if (status === 'completed' && conclusion === 'failed') {
-        console.log(`monkaS 👉 pipeline in ${repository} on branch ${branch} failed @helltf`)
+	} else if (status === 'completed' && conclusion === 'failure') {
+		hb.client.say(
+			'helltf',
+			`monkaS 👉 pipeline in ${repository} on branch ${branch} failed @helltf`
+		)
 	}
 }
 
