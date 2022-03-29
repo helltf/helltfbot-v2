5a694fd5e215523202a56bf042622387b4edae32
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 17:53:37 +0200

    added test func

--- helltfbot.ts
+++ helltfbot.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config'
+import { getData } from './jobs/github-pipeline.js'
 import { TwitchBot } from './client/bot.js'
 import { mainClient } from './client/main-bot.js'
 import { watchClient } from './client/track-bot.js'
 
 const hb = new TwitchBot(mainClient, watchClient)
+getData()
 
 export{ hb }
