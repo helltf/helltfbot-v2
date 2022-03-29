38eab8f055037da3c1b30ecbd84fb80a89495332
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 13:14:24 +0200

    set to identify options

--- config/config.ts
+++ config/config.ts
@@ -1,45 +1,11 @@
-import { Options } from 'tmi.js'
-
-const getMainClientConfig = (): Options => {
-	return new BotOptions()
-}
-
-const getTrackClientConfig = (): Options => {
-	return new BotOptions()
-}
-
-class BotOptions implements Options {
-	channels?: string[]
-	connection?: {
-		server?: string
-		port?: number
-		reconnect?: boolean
-		maxReconnectAttempts?: number
-		maxReconnectInverval?: number
-		reconnectDecay?: number
-		reconnectInterval?: number
-		secure?: boolean
-		timeout?: number
-	}
-	identity?: {
-		username?: string
-		password?: string | (() => string | Promise<string>)
-	}
-	logger?: {
-		info: (message: string) => any
-		warn: (message: string) => any
-		error: (message: string) => any
-	}
-	options?: {
-		clientId?: string
-		debug?: boolean
-		joinInterval?: number
-		globalDefaultChannel?: string
-		messagesLogLevel?: string
-		skipMembership?: boolean
-		skipUpdatingEmotesets?: boolean
-		updateEmotesetsTimer?: number
+class IdentityOptions {
+	username: string
+	password: string
+	
+	constructor(password: string, username: string){
+		this.password = "oauth:" + password
+		this.username = username
 	}
 }
 
-export { getMainClientConfig, getTrackClientConfig }
+export { IdentityOptions }
