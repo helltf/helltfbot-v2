d2c45ca7f7e2dae97efd75faf2eb0a5b2c1742e5
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 19:38:35 +0200

    added init function

--- helltfbot.ts
+++ helltfbot.ts
@@ -4,9 +4,10 @@ import { TwitchBot } from './client/bot.js'
 import { mainClient } from './client/main-bot.js'
 import { watchClient } from './client/track-bot.js'
 
-const hb = new TwitchBot(mainClient, watchClient)
-
+const hb = await new TwitchBot(mainClient, watchClient).init()
 
 updateGithubPipeline()
+
 setInterval(updateGithubPipeline, 10000)
-export{ hb }
+
+export { hb }
