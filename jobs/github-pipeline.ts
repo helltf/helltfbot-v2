f90f7d4f25f369e8de970130eaee77e40d15cba0
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 18:24:47 +0200

    check for success

--- jobs/github-pipeline.ts
+++ jobs/github-pipeline.ts
@@ -2,8 +2,11 @@ import { getPipeLineData } from '../api/github/github-api.js'
 import { Projects } from '../api/github/github-projects.js'
 
 const getData = async () => {
-    let res = await getPipeLineData(Projects.helltfbot_v2)
-    console.log(res)
+    let {success, data, error} = await getPipeLineData(Projects.helltfbot_v2)
+    
+    if(!success) return
+
+    console.log(data)
 }
 
 export {getData}
