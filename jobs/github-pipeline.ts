54919172e70d67c542bad21b61c245bd4787334a
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 18:40:19 +0200

    check for update and log success or failed

--- jobs/github-pipeline.ts
+++ jobs/github-pipeline.ts
@@ -1,12 +1,34 @@
-import { getPipeLineData } from '../api/github/github-api.js'
+import { getPipeLineData, PipelineData } from '../api/github/github-api.js'
 import { Projects } from '../api/github/github-projects.js'
+let count: number = undefined
 
 const getData = async () => {
-    let {success, data, error} = await getPipeLineData(Projects.helltfbot_v2)
-    
-    if(!success) return
+	let { success, data, error } = await getPipeLineData(Projects.helltfbot_v2)
 
-    console.log(data)
+	if (!success) return
+
+	if (count === undefined) {
+		count = data.count
+	}
+
+	if (count !== data.count) {
+		announceNewPipeLine(data)
+	}
 }
 
-export {getData}
+function announceNewPipeLine({
+	status,
+	conclusion,
+	repository,
+	branch,
+}: PipelineData) {
+	if (status === 'completed' && conclusion === 'success') {
+		console.log(
+			`catJAM 👉 pipeline in ${repository} on branch ${branch} was successful`
+		)
+	} else if (status === 'completed' && conclusion === 'failed') {
+        console.log(`monkaS 👉 pipeline in ${repository} on branch ${branch} failed @helltf`)
+	}
+}
+
+export { getData }
