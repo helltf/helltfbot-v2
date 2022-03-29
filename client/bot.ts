cefb3945c8d3f895ce45d25f94958e3ffff83dbc
helltf <Dario.Zachow5530@gmail.com>
2022-03-29 19:38:51 +0200

    added init function

--- client/bot.ts
+++ client/bot.ts
@@ -1,17 +1,18 @@
-import {Client} from 'tmi.js'
+import { Client } from 'tmi.js'
 
 class TwitchBot {
-    client: Client
-    watchclient: Client
-    commands: string[]
-    constructor(client: Client, watchclient: Client){
-        this.client = client
-        this.watchclient = watchclient
-
-        this.client.connect()
-        this.watchclient.connect()
-
-    }
+	client: Client
+	watchclient: Client
+	commands: string[]
+	constructor(client: Client, watchclient: Client) {
+		this.client = client
+		this.watchclient = watchclient
+	}
+	async init(): Promise<TwitchBot> {
+		this.client.connect()
+		this.watchclient.connect()
+        return this
+	}
 }
 
-export {TwitchBot}
+export { TwitchBot }
