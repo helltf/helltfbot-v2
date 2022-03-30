fe64431eb4954b911cc4a6f805365b31dfa01138
helltf <Dario.Zachow5530@gmail.com>
2022-03-30 16:44:28 +0200

    added handler for receiving and sending messages

--- client/main-bot.ts
+++ client/main-bot.ts
@@ -1,25 +1,57 @@
 import { ChatUserstate, Client } from 'tmi.js'
 import * as tmi from 'tmi.js'
 import { IdentityOptions } from '../config/config.js'
+import { hb } from '../helltfbot.js'
+import { BotResponse } from './bot.js'
 
 const mainClient = createMainClient()
+const prefix = process.env.PREFIX
+const DEFAULT_ERROR = `Error while executing your command monkaS`
 
-function createMainClient (): Client {
-    let clientOptions = new IdentityOptions(process.env.TWITCH_OAUTH, "helltfbot")
-    return tmi.Client({identity: clientOptions})
+
+function createMainClient(): Client {
+	let clientOptions = new IdentityOptions(process.env.TWITCH_OAUTH, 'helltfbot')
+	return tmi.Client({ identity: clientOptions })
 }
-
 
 mainClient.on(
 	'chat',
-	(
+	async (
 		channel: string,
 		userstate: ChatUserstate,
 		message: string,
 		self: boolean
 	) => {
-    }
+		if (self) return
+		if (!message?.toLowerCase()?.startsWith(prefix)) return
+
+		channel = channel.replace('#', '')
+
+		let [commandName, ...data] = message
+			.substring(prefix.length)
+			.replace(/\s{2,}/g, ' ')
+			.split(' ')
+
+		let command = hb.commands.get(commandName)
+
+		if(command === undefined) return
+
+		let response = await command.execute(channel, userstate, data)
+
+		sendResponse(response)
+	}
 )
 
+function sendMessage(channel: string, message: string){
+	mainClient.say(channel, message)
+}
+
+function sendResponse({success, response, channel}: BotResponse){
+	if(success){
+		sendMessage(channel, response)
+	}else{
+		mainClient.say(channel, DEFAULT_ERROR)
+	}
+}
 
 export { mainClient }
