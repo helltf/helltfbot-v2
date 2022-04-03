1bf5f414fdba9a18b4bbcf5651d3d8371c261ce0
helltf <Dario.Zachow5530@gmail.com>
2022-04-03 22:21:22 +0200

    added helper functions

--- commands/export/cooldown.ts
+++ commands/export/cooldown.ts
@@ -1,25 +1,46 @@
-import { Command } from "./command"
+import { Command } from './command'
 
-export class Cooldown{
+export class Cooldown {
 	cooldowns: Map<string, string[]>
 
 	constructor() {
 		this.cooldowns = new Map<string, string[]>()
 	}
 
-	setCooldown(command: Command, userId: string){
-        let cooldowns = this.getCooldownsForUser(userId)
-        cooldowns.push("fsjdlkfsjfdl")
-		setTimeout(()=> {
+	setCooldown({ name, cooldown }: Command, userId: string) {
+		this.#addCooldown(userId, name)
 
-			if(cooldowns.includes(command.name)){
-				this.cooldowns.delete
-			}
-
-		}, command.cooldown)
+		setTimeout(() => {
+			this.#removeCooldown(userId, name)
+		}, cooldown)
 	}
 
-	getCooldownsForUser(userId: string): string[]{
+	getCooldownsForUser(userId: string): string[] {
 		return this.cooldowns.get(userId)
+	}
+
+	#removeCooldown(userId: string, commandName: string) {
+		let userCooldowns = this.getCooldownsForUser(userId)
+
+		if (userCooldowns.includes(commandName)) {
+			this.cooldowns.set(
+				userId,
+				userCooldowns.filter((e) => e !== commandName)
+			)
+		}
+	}
+
+	#addCooldown(userId: string, comamndName: string) {
+		let userCooldowns = this.getCooldownsForUser(userId)
+
+		if (!userCooldowns) {
+			this.cooldowns.set(userId, [comamndName])
+		} else {
+			userCooldowns.push(comamndName)
+		}
+	}
+
+	userHasCooldown({ name }: Command, userId: string): boolean {
+		return this.getCooldownsForUser(userId).includes(name)
 	}
 }
