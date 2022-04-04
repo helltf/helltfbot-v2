aaae175335aecfd925eb74ff8443fb73dc2759c4
helltf <Dario.Zachow5530@gmail.com>
2022-04-04 22:25:16 +0200

    implemented execution of all jobs

--- helltfbot.ts
+++ helltfbot.ts
@@ -5,6 +5,7 @@ import { mainClient } from './client/main-bot.js'
 import { watchClient } from './client/track-bot.js'
 import { loadCommands } from './commands/export/export-commands.js'
 import { dbClient } from './database/database-init.js'
+import jobs from 'jobs/jobs-export.js'
 
 const hb = await new TwitchBot(mainClient, watchClient, dbClient).init()
 hb.setCommands(await loadCommands())
@@ -18,7 +19,11 @@ startJobs()
 function startJobs() {
 	if (process.env.NODE_ENV === 'dev') return
 
-	setInterval(updateGithubPipeline, 60000)
+	for (let {delay, execute} of jobs){
+		execute()
+		setInterval(execute, delay)
+	}
+
 }
 
 export { hb }
