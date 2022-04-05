482e66f6b69d72fc190247ca3a4c8698773546f7
helltf <Dario.Zachow5530@gmail.com>
2022-04-05 20:16:34 +0200

    removed db content from twitchbot object

--- database/database-init.ts
+++
@@ -1,7 +0,0 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const { PrismaClient } = require("@prisma/client");
-
-const dbClient = new PrismaClient();
-
-export {dbClient}
