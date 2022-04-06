d039be9f2c351f51cdb438e24db1fdeaf9258547
helltf <Dario.Zachow5530@gmail.com>
2022-04-06 21:32:57 +0200

    removed unused function

--- db/export-orm.ts
+++ db/export-orm.ts
@@ -1,10 +1,4 @@
-import { DataSource, Repository } from "typeorm";
+import { DataSource} from "typeorm";
 import {typeormconf} from '../ormconfig.js'
 
-const initDbRepositories = () => {
-    return {
-
-    }
-}
 export const AppDataSource = new DataSource(typeormconf)
-export {initDbRepositories}
