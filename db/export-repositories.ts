814c8c1d257d9d7914d307cda2b3a98455086348
helltf <Dario.Zachow5530@gmail.com>
2022-04-08 19:16:37 +0200

    added new channel repository

--- db/export-repositories.ts
+++ db/export-repositories.ts
@@ -1,16 +1,19 @@
 import { Repository } from "typeorm"
+import { Channel } from "./entity/channel.js"
 import { ColorHistory } from "./entity/color_history.js"
 import { WordleWord } from "./entity/wordle.js"
 import { AppDataSource } from "./export-orm.js"
 
 const repositories = {
     wordleRepo: AppDataSource.getRepository(WordleWord),
-    colorRepo: AppDataSource.getRepository(ColorHistory)
+    colorRepo: AppDataSource.getRepository(ColorHistory),
+    channelRepo: AppDataSource.getRepository(Channel)
 }
 
 export interface DbRepositories{
     wordleRepo: Repository<WordleWord>
     colorRepo: Repository<ColorHistory>
+    channelRepo: Repository<Channel>
 }
 
 export {repositories}
