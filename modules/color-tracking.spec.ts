d4e8f9673281e9483b01f1fdecb34ca3f26046d9
helltf <Dario.Zachow5530@gmail.com>
2022-04-09 15:02:18 +0200

    added unit tests for colors

--- modules/color-tracking.spec.ts
+++ modules/color-tracking.spec.ts
@@ -1,9 +1,41 @@
-import { updateCurrentColors } from "./color-tracking"
+import 'jasmine'
+import { updateCurrentColors } from './color-tracking.js'
 
-test('update color pushs new color', () => {
-    let initialArray = []
-    let colorToAdd = '#ffffff'
-    let result = updateCurrentColors(initialArray, colorToAdd)
+describe('color tracking tests', () => {
+	let examples = {
+		a: '#aaaaaa',
+		b: '#bbbbbb',
+		c: '#cccccc',
+		d: '#dddddd',
+		e: '#eeeeee',
+		f: '#ffffff',
+	}
 
-    expect(result).toBe([colorToAdd])
+	let colorToAdd = '#zzzzzz'
+
+	it('update color pushs new color', () => {
+		let initialArray = []
+
+		let result = updateCurrentColors(initialArray, colorToAdd)
+		let expected = [colorToAdd]
+
+		expect(result).toEqual(expected)
+	})
+
+	it('update color pushs new color', () => {
+		let initialArray = [examples.a, examples.b]
+
+		let result = updateCurrentColors(initialArray, colorToAdd)
+		let expected = [examples.a, examples.b, colorToAdd]
+
+		expect(result).toEqual(expected)
+	})
+
+	it('add new existing color, changes position', () => {
+		let initialArray = [examples.a, colorToAdd, examples.c]
+
+		let result = updateCurrentColors(initialArray, colorToAdd)
+		let expected = [examples.a, examples.c, colorToAdd]
+		expect(result).toEqual(expected)
+	})
 })
