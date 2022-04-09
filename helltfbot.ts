7a316f71e77032097c89c6fd3b69c67aa4979f18
helltf <Dario.Zachow5530@gmail.com>
2022-04-09 11:20:19 +0200

    moved functions into bot

--- helltfbot.ts
+++ helltfbot.ts
@@ -1,36 +1,19 @@
 import 'dotenv/config'
 import { TwitchBot } from './client/bot.js'
-import { mainClient, mainJoinAllChannels } from './client/main-bot.js'
+import { mainClient } from './client/main-bot.js'
 import { watchClient } from './client/track-bot.js'
-import { loadCommands } from './commands/export/export-commands.js'
+import commands from './commands/export/export-commands.js'
 import { AppDataSource } from './db/export-orm.js'
-import jobs from './jobs/jobs-export.js'
 import { repositories } from './db/export-repositories.js'
-import { init } from './modules/color-tracking.js'
+
 
 await AppDataSource.initialize()
 const hb = await new TwitchBot(mainClient, watchClient).init()
-hb.setCommands(await loadCommands())
+
+hb.setCommands(commands)
 hb.setRepositories(repositories)
-
-await hb.client.join(process.env.MAIN_USER)
-await hb.watchclient.join(process.env.MAIN_USER)
-
-mainJoinAllChannels()
-startJobs()
-initModules()
-
-function startJobs() {
-	if (process.env.NODE_ENV === 'dev') return
-
-	for (let { delay, execute } of jobs) {
-		execute()
-		setInterval(execute, delay)
-	}
-}
-
-function initModules(){
-	init()
-}
+hb.joinChannels()
+hb.startJobs()
+hb.initModules()
 
 export { hb }
