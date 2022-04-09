ff0922a67197c0df1ef908d081ecffd2c3139d37
helltf <Dario.Zachow5530@gmail.com>
2022-04-09 20:51:07 +0200

    added file to get permissions of user

---
+++ utilities/twitch/permission.ts
@@ -0,0 +1,26 @@
+import { Userstate, Badges } from "tmi.js"
+import { hb } from "../../helltfbot.js"
+
+const getChatPermissions = (badges: Badges): number => {
+    if(badges.broadcaster !== undefined) return 1
+    if(badges.moderator !== undefined) return 2
+    if(badges.vip !== undefined) return 3
+    if(badges.subscriber !== undefined || badges.founder !== undefined) return 4
+    return 5
+}
+
+const returnCorrectPermissions = (db: number, chat: number) => {
+    return (db < chat) ? db: chat
+}
+
+const getUserPermissions = async(user: Userstate): Promise<number> => {
+    const chatPermissions = getChatPermissions(user.badges)
+    const dbPermissions = (await hb.db.userRepo.findOneBy({
+        id: Number(user["user-id"])
+    }))?.permission
+
+    return returnCorrectPermissions(chatPermissions, dbPermissions)
+}
+
+
+export{ getChatPermissions, returnCorrectPermissions, getUserPermissions}
