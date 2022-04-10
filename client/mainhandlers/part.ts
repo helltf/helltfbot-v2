cb82769159aa434ead9f1493a97358915b1b7af0
helltf <Dario.Zachow5530@gmail.com>
2022-04-10 12:15:25 +0200

    implemented handling of parts of mainclient

--- client/mainhandlers/part.ts
+++ client/mainhandlers/part.ts
@@ -1,11 +1,19 @@
-import { hb } from "../../helltfbot.js"
+import { hb } from '../../helltfbot.js'
 
 hb.client.on('part', (channel: string, username: string, self: boolean) => {
-	if(!self) return
-
+	if (!self) return
+	channel = channel.replace('#', '')
+	
 	handlePart(channel, username)
 })
 
-function handlePart(channel: string, username: string){
-    
+function handlePart(channel: string, username: string) {
+	hb.db.channelRepo.update(
+		{
+			channel: channel,
+		},
+		{
+			joined: false,
+		}
+	)
 }
