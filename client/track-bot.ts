17ded632229a06d1a579b6fa8714d6c100046957
helltf <Dario.Zachow5530@gmail.com>
2022-04-10 12:40:03 +0200

    organized imports

--- client/track-bot.ts
+++ client/track-bot.ts
@@ -1,7 +1,5 @@
 import { IdentityOptions } from '../config/config.js'
 import { Client } from 'tmi.js'
-import { hb } from '../helltfbot.js'
-import { wait } from '../utilities/timeout.js'
 
 const watchClient = createWatchClient()
 
