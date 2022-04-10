be47671142221547f90ab50f3a149e9a2cd97453
helltf <Dario.Zachow5530@gmail.com>
2022-04-10 18:44:02 +0200

    Revert "fixed?"
    This reverts commit 56ade0f1cc7486ae613195c2b553ffb96d2ed8d2.

--- helltfbot.ts
+++ helltfbot.ts
@@ -6,14 +6,20 @@ import commands from './commands/export/export-commands.js'
 import { AppDataSource } from './db/export-orm.js'
 import { repositories } from './db/export-repositories.js'
 
+
 const hb = new TwitchBot(mainClient, watchClient)
 hb.setCommands(commands)
 hb.setRepositories(repositories)
 
-await AppDataSource.initialize()
-await hb.init()
-hb.joinChannels()
-hb.startJobs()
-hb.initModules()
+const start = async() => {
+	if(process.env.NODE_ENV === 'test') return;
+	await AppDataSource.initialize()
+	await hb.init()
+	hb.joinChannels()
+	hb.startJobs()
+	hb.initModules()
+}
+
+start()
 
 export { hb }
