17610497376100e80e6089098e4b1cae419a2aef
helltf <Dario.Zachow5530@gmail.com>
2022-04-10 12:15:43 +0200

    moved from track bot file

---
+++ client/watchhandlers/join.ts
@@ -0,0 +1,22 @@
+import { hb } from "../../helltfbot.js"
+import { wait } from "../../utilities/timeout.js"
+import { watchClient } from "../track-bot.js"
+
+async function watchJoinAllChannels(){
+    if(process.env.NODE_ENV === 'dev'){
+        watchClient.join(process.env.MAIN_USER)
+        return
+    }
+
+    let channels = await hb.db.watchRepo.find()
+    
+    for await (let {channel} of channels){
+        await watchClient.join(channel)
+        await wait`1s`
+    }
+
+    hb.log(`Successfully joined ${channels.length} channels to watch`)
+}
+
+
+export { watchJoinAllChannels }
