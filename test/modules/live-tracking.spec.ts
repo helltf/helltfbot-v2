9eebeb7a2d6c34e1633cf11bf1d058b4eb11f468
helltf <Dario.Zachow5530@gmail.com>
2022-04-26 22:32:09 +0200

    added more test suites

--- test/modules/live-tracking.spec.ts
+++ test/modules/live-tracking.spec.ts
@@ -1,18 +1,38 @@
-import { chunkTopicsInto50 } from "../../modules/live-tracking.js"
+import { chunkTopicsIntoSize } from "../../modules/live-tracking.js"
 
 describe('tests for live tracking module', () => {
 
     it('array reduce should return 0 entries' , () => {
-        const channels = new Map()
-        const result = chunkTopicsInto50(channels)
+        const channels = []
+        const result = chunkTopicsIntoSize(channels)
         const expectedSize = 0
 
         expect(result).toHaveSize(expectedSize)
     })
 
     it('array reduce should return 0 entries' , () => {
-        const channels = new Map([[1,'user1']])
+        const channels = [{id: 1, name: 'user1'}]
+        const expectedResult = [[{id: 1, name: 'user1'}]]
 
-        const result = chunkTopicsInto50(channels)
+        const result = chunkTopicsIntoSize(channels)
+
+        expect(result).toEqual(expectedResult)
+    })
+    it('array reduce should return 0 entries' , () => {
+        const channels = [{id: 1, name: 'user1'}, {id: 2, name: 'user2'}, {id: 3, name: 'user3'} ]
+        const expectedLength = 1
+
+        const result = chunkTopicsIntoSize(channels)
+
+        expect(result).toHaveSize(expectedLength)
+    })
+    it('array reduce should return 0 entries' , () => {
+        let maxArraySize = 1
+        const channels = [{id: 1, name: 'user1'}, {id: 2, name: 'user2'}, {id: 3, name: 'user3'} ]
+        const expectedLength = 3
+
+        const result = chunkTopicsIntoSize(channels, maxArraySize)
+
+        expect(result).toHaveSize(expectedLength)
     })
 })
