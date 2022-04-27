3b1ac9e5db233aaf96c7add78f42b29887b59542
helltf <Dario.Zachow5530@gmail.com>
2022-04-27 16:09:32 +0200

    created eventhandler class to handle incoming messages

--- modules/live_tracking/update-event-handler.ts
+++ modules/live_tracking/update-event-handler.ts
@@ -1,23 +1,88 @@
-import { hb } from "../../helltfbot.js";
-import { PubSubData, SettingMessage, StatusMessage, UpdateEventType } from "./types.js";
+import { title } from 'process'
+import { hb } from '../../helltfbot.js'
+import {
+	PubSubData,
+	PubSubMessageEventType,
+	SettingMessage,
+} from './types.js'
 
-export class UpdateEventHandler{
-    constructor(){}
+export class UpdateEventHandler {
+	constructor() {}
 
-    handleSettingUpdateEvent({ message }: PubSubData<SettingMessage>, streamer: string){
-        console.log('Setting update:' + streamer)
-        if(message.old_status !== message.status){
-            this.sendMessage('helltf', `new title in ${streamer}: ${message.status}`)
-        }
-    
-        if(message.old_game !== message.game){
-            this.sendMessage('helltf', `new game in ${streamer}: ${message.game}`)
-        }
-    }
-    handleStatusEvent(data: PubSubData<StatusMessage>, streamer: string){
-        this.sendMessage('helltf', `${streamer} is live`)
-    }
-    sendMessage(channel: string, message: string){
-        hb.sendMessage(channel, message)
-    }
+	handleSettingUpdateEvent(
+		{ message }: PubSubData<SettingMessage>,
+		streamer: string
+	): Map<string, string[]> {
+		let messages: Map<string, string[]> = new Map()
+
+		if (message.old_status !== message.status) {
+			messages = this.addMessageToEntry(
+				messages,
+				`${streamer} has changed the title to ${message.status}`,
+				'helltf'
+			)
+		}
+
+		if (message.old_game !== message.game) {
+			messages = this.addMessageToEntry(
+				messages,
+				`${streamer} has changed the game to ${message.game}`,
+				'helltf'
+			)
+		}
+
+		return messages
+	}
+
+	handleLiveEvent(streamer: string): Map<string, string[]> {
+		return new Map([['helltf', [`${streamer} has gone live`]]])
+	}
+
+	handleOfflineEvent(streamer: string): Map<string, string[]> {
+		return new Map([['helltf', [`${streamer} has gone offline`]]])
+	}
+
+	handleUpdate(
+		data: PubSubData<any>,
+		streamer: string,
+		type: PubSubMessageEventType
+	): Map<string, string[]> {
+		if (type === 'stream-up') {
+			return this.handleLiveEvent(streamer)
+		}
+
+		if (type === 'stream-down') {
+			return this.handleOfflineEvent(streamer)
+		}
+
+		if (type === 'broadcast_settings_update') {
+			return this.handleSettingUpdateEvent(data, streamer)
+		}
+	}
+
+	addMessageToEntry(
+		map: Map<string, string[]>,
+		newMessage: string,
+		channel: string
+	): Map<string, string[]> {
+		let entry = map.get(channel)
+
+		if (entry) {
+			entry.push(newMessage)
+		} else {
+			map.set(channel, [newMessage])
+		}
+
+		return map
+	}
+}
+
+export class NotificationHandler {
+	constructor() {}
+
+	sendNotifications(notifications: Map<string, string[]>) {
+		for (let [channel, messages] of notifications) {
+			messages.forEach(m => hb.sendMessage(channel, m))
+		}
+	}
 }
