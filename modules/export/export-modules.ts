f08e3612731605bcb42df54d6835396bc40627b5
helltf <Dario.Zachow5530@gmail.com>
2022-04-27 21:53:28 +0200

    added ban module as class

--- modules/export/export-modules.ts
+++ modules/export/export-modules.ts
@@ -1,4 +1,4 @@
-import { banModule } from '../ban-tracking.js'
+import { BanTracking } from '../ban-tracking.js'
 import { ColorTracking } from '../color-tracking.js'
 import { LiveTracking } from '../live_tracking/live-tracking.js'
 import { TimeoutTracking } from '../timeout-tracking.js'
@@ -6,5 +6,6 @@ import { Module } from './module.js'
 const liveTracking = new LiveTracking()
 const timeoutModule = new TimeoutTracking()
 const colorModule = new ColorTracking()
+const banModule = new BanTracking()
 const modules: Module[] = [timeoutModule, banModule, colorModule, liveTracking]
 export { modules }
