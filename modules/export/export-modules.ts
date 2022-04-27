0c378d6fa79ab3cc59d8deebcf8e18f1d0822fd0
helltf <Dario.Zachow5530@gmail.com>
2022-04-27 16:08:05 +0200

    refactored module into class

--- modules/export/export-modules.ts
+++ modules/export/export-modules.ts
@@ -1,8 +1,9 @@
 import { banModule } from '../ban-tracking.js'
 import { initializeColorTracking } from '../color-tracking.js'
-import { liveTrackingModule } from '../live_tracking/live-tracking.js'
+import { LiveTracking } from '../live_tracking/live-tracking.js'
 import { timeoutModule } from '../timeout-tracking.js'
 import { Module } from './module.js'
+const liveTracking = new LiveTracking()
 
-const modules: Module[] = [timeoutModule, banModule, {initialize: initializeColorTracking}, liveTrackingModule]
+const modules: Module[] = [timeoutModule, banModule, {initialize: initializeColorTracking}, liveTracking]
 export { modules }
--- modules/live_tracking/live-tracking.ts
+++ modules/live_tracking/live-tracking.ts
@@ -5,17 +5,12 @@ import { Module } from '../export/module.js'
 import {
 	WebSocketConnection,
 	PubSubChannel,
-	PubSubData,
 	PubSubType,
 	PubSubMessage,
 	TopicType,
-	SettingMessage,
-	StatusMessage,
+	PubSubMessageEventType,
 } from './types.js'
-import { UpdateEventHandler } from './update-event-handler.js'
-
-const updateEventHandler = new UpdateEventHandler()
-const connections: WebSocketConnection[] = []
+import { NotificationHandler, UpdateEventHandler } from './update-event-handler.js'
 
 let channels: PubSubChannel[] = [
 	{ id: 109035947, name: 'helltf' },
@@ -24,129 +19,133 @@ let channels: PubSubChannel[] = [
 	{ id: 31545223, name: 'agurin' },
 ]
 
-const setPingInterval = (con: ReconnectingWebSocket): NodeJS.Timer => {
-	return setInterval(() => {
-		con.send(
-			JSON.stringify({
-				type: 'PING',
-			})
-		)
-	}, 250 * 1000)
-}
+export class LiveTracking implements Module {
+	channels: PubSubChannel[]
+	updateEventHandler: UpdateEventHandler
+	connections: WebSocketConnection[]
+	notificationHandler: NotificationHandler
 
-function handleIncomingMessage({ data }: any) {
-	if (!data?.message) return
-
-	data.message = JSON.parse(data.message)
-
-	if (!data.message) return
-
-	let name = getNameForTopic(data.topic)
-
-	if(data.message.type === 'stream-up' || data.message.type === 'stream-down'){
-		updateEventHandler.handleStatusEvent(data, name)
+	constructor() {
+		this.updateEventHandler = new UpdateEventHandler()
+		this.notificationHandler = new NotificationHandler()
+		this.connections = []
 	}
 
-	if(data.message.type === 'broadcast_settings_update'){
-		updateEventHandler.handleSettingUpdateEvent(data, name)
+	async initialize() {
+		this.channels = channels
+		this.connectPubSub()
 	}
-	
-}
-const connectPubSub = async () => {
-	const chunkedChannels = chunkTopicsIntoSize(channels)
 
-	for await (let channels of chunkedChannels) {
-		const connection = new RWS.default('wss://pubsub-edge.twitch.tv', [], {
-			WebSocket: WS.WebSocket,
-		})
+	setPingInterval(con: ReconnectingWebSocket): NodeJS.Timer {
+		return setInterval(() => {
+			con.send(
+				JSON.stringify({
+					type: 'PING',
+				})
+			)
+		}, 250 * 1000)
+	}
 
-		connection.addEventListener('message', ({ data }) => {
-			handlePubSubMessage(JSON.parse(data))
-		})
+	handleIncomingMessage({ data }: any) {
+		if (!data?.message) return
+		data.message = JSON.parse(data.message)
 
-		let conInterval = setPingInterval(connection)
+		if (!data.message) return
+		let type = data.message.type
+		let streamer = this.getNameForTopic(data.topic)
 
-		connection.addEventListener('open', () => {
-			connections.push({
-				connection: connection,
-				interval: conInterval,
+		if (
+			type === 'stream-up' || type === 'stream-down' || type === 'broadcast_settings_update'
+		) {
+			let messages = this.updateEventHandler.handleUpdate(data, streamer, type)
+			this.notificationHandler.sendNotifications(messages)
+		}
+
+	}
+
+	connectPubSub = async () => {
+		const chunkedChannels = this.chunkTopicsIntoSize(channels)
+
+		for await (let channels of chunkedChannels) {
+			const connection = new RWS.default('wss://pubsub-edge.twitch.tv', [], {
+				WebSocket: WS.WebSocket,
 			})
-		})
 
-		for await (let { id } of channels) {
-			await startListenToTopics(connection, id)
-			await wait`5s`
+			connection.addEventListener('message', ({ data }) => {
+				this.handlePubSubMessage(JSON.parse(data))
+			})
+
+			let conInterval = this.setPingInterval(connection)
+
+			connection.addEventListener('open', () => {
+				this.connections.push({
+					connection: connection,
+					interval: conInterval,
+				})
+			})
+
+			for await (let { id } of channels) {
+				await this.startListenToTopics(connection, id)
+				await wait`5s`
+			}
 		}
 	}
-}
 
-const handlePubSubMessage = (data: any) => {
-	let type: PubSubType = data.type
+	handlePubSubMessage = (data: any) => {
+		let type: PubSubType = data.type
 
-	if (type !== 'MESSAGE') return
+		if (type !== 'MESSAGE') return
 
-	handleIncomingMessage(data)
-}
-
-const sendMessage = (con: ReconnectingWebSocket, message: PubSubMessage) => {
-	con.send(JSON.stringify(message))
-}
-
-const startListenToTopics = async (
-	con: ReconnectingWebSocket,
-	channelId: number
-) => {
-	const liveMessage = createMessageForTopic(TopicType.LIVE, channelId)
-	const infoMessage = createMessageForTopic(TopicType.INFO, channelId)
-
-	sendMessage(con, liveMessage)
-
-	await wait`1s`
-
-	sendMessage(con, infoMessage)
-}
-
-const createMessageForTopic = (
-	topic: TopicType,
-	channelId: number
-): PubSubMessage => {
-	return {
-		type: 'LISTEN',
-		nonce: '',
-		data: {
-			auth_token: process.env.TWITCH_OAUTH,
-			topics: [`${topic}${channelId}`],
-		},
+		this.handleIncomingMessage(data)
 	}
-}
-const chunkTopicsIntoSize = (
-	arr: PubSubChannel[],
-	size: number = 25
-): PubSubChannel[][] => {
-	return arr.reduce(
-		(acc, _, i) => (i % size ? acc : [...acc, arr.slice(i, i + size)]),
-		[]
-	)
-}
-function getNameForTopic(topic: string): string {
-	let id = getIdForTopic(topic)
 
-	let { name } = channels.find((c) => c.id === parseInt(id))
+	sendMessage = (con: ReconnectingWebSocket, message: PubSubMessage) => {
+		con.send(JSON.stringify(message))
+	}
 
-	return name
-}
+	startListenToTopics = async (
+		con: ReconnectingWebSocket,
+		channelId: number
+	) => {
+		const liveMessage = this.createMessageForTopic(TopicType.LIVE, channelId)
+		const infoMessage = this.createMessageForTopic(TopicType.INFO, channelId)
 
-function getIdForTopic(topic: string): string {
-	return topic.match(/(?<=\.)\d+/gim)[0]
-}
+		this.sendMessage(con, liveMessage)
 
-const liveTrackingModule: Module = {
-	initialize: connectPubSub,
-}
+		await wait`1s`
 
-export {
-	liveTrackingModule,
-	chunkTopicsIntoSize,
-	getNameForTopic,
-	getIdForTopic,
+		this.sendMessage(con, infoMessage)
+	}
+	createMessageForTopic = (
+		topic: TopicType,
+		channelId: number
+	): PubSubMessage => {
+		return {
+			type: 'LISTEN',
+			nonce: '',
+			data: {
+				auth_token: process.env.TWITCH_OAUTH,
+				topics: [`${topic}${channelId}`],
+			},
+		}
+	}
+	chunkTopicsIntoSize = (
+		arr: PubSubChannel[],
+		size: number = 25
+	): PubSubChannel[][] => {
+		return arr.reduce(
+			(acc, _, i) => (i % size ? acc : [...acc, arr.slice(i, i + size)]),
+			[]
+		)
+	}
+	getNameForTopic(topic: string): string {
+		let id = this.getIdForTopic(topic)
+
+		let { name } = channels.find((c) => c.id === parseInt(id))
+
+		return name
+	}
+	getIdForTopic(topic: string): string {
+		return topic.match(/(?<=\.)\d+/gim)[0]
+	}
 }
