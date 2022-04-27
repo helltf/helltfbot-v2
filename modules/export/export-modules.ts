0a43d47af1d79d604b0b96285ebb13004a905032
helltf <Dario.Zachow5530@gmail.com>
2022-04-27 12:53:56 +0200

    updated import

--- modules/export/export-modules.ts
+++ modules/export/export-modules.ts
@@ -1,6 +1,6 @@
 import { banModule } from '../ban-tracking.js'
 import { initializeColorTracking } from '../color-tracking.js'
-import { liveTrackingModule } from '../live-tracking.js'
+import { liveTrackingModule } from '../live_tracking/live-tracking.js'
 import { timeoutModule } from '../timeout-tracking.js'
 import { Module } from './module.js'
 
