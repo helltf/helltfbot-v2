94bb7b07a8e56d58c59a450190e3720f353e1ac2
helltf <Dario.Zachow5530@gmail.com>
2022-04-28 22:08:52 +0200

    created interface for better testability

--- db/entity/notification_channel.ts
+++ db/entity/notification_channel.ts
@@ -1,10 +1,15 @@
 import { BaseEntity, Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('notification_channel')
-export class NotificationChannel extends BaseEntity{
+export class NotificationChannel extends BaseEntity implements NotificationChannelInfo {
     @PrimaryColumn('int')
     id: number
 
     @Column('varchar')
+    name: string
+}
+
+export interface NotificationChannelInfo{
+    id: number
     name: string
 }
