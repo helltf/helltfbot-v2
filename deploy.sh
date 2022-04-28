26fc4cd1d817beb70574c8e76e8ea7761464f305
helltf <Dario.Zachow5530@gmail.com>
2022-04-28 16:56:35 +0200

    set name to stop container by name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=helltfbot-v2 --format="{{.ID}}"))
+docker stop helltfbot-v2
 
 docker build -f ./Dockerfile . -t helltfbot-v2
 
-docker run -d --restart unless-stopped --network="host" helltfbot-v2
+docker run -d --restart unless-stopped --network="host" --name helltfbot-v2 helltfbot-v2
