a915c919266282eae3f4a7e7e634b85e60f50046
helltf <Dario.Zachow5530@gmail.com>
2022-04-28 16:52:59 +0200

    updated script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=helltfbot-v2 --format="{{.ID}}"))
 
 docker build -f ./Dockerfile . -t helltfbot-v2
 
-docker run -d --network="host" helltfbot-v2
+docker run -d --restart unless-stopped --network="host" helltfbot-v2
