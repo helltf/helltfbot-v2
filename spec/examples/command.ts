a66721e99113d1de4039480ba1be9f986ae44094
helltf <Dario.Zachow5530@gmail.com>
2022-04-29 15:37:44 +0200

    implement interface instead of class

--- spec/examples/command.ts
+++ spec/examples/command.ts
@@ -1,12 +1,14 @@
-import { BotResponse } from "../../client/response.js";
-import { Command } from "../../commands/export/types.js";
+import { BotResponse } from '../../client/response.js'
+import { Command } from '../../commands/export/types.js'
 
 export const exampleCommand = new Command({
-    name: 'example',
-    cooldown: 1000,
-    description: 'example',
-    execute: async () => new BotResponse('', '', true),
-    optionalParams: [],
-    requiredParams: [],
-    permissions: 5,
+	name: 'example',
+	cooldown: 1000,
+	description: 'example',
+	execute: async () => {
+		return { response: '', channel: '', success: true }
+	},
+	optionalParams: [],
+	requiredParams: [],
+	permissions: 5,
 })
