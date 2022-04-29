671d174d1f970233d1d821cf32fea2beeec5312b
helltf <Dario.Zachow5530@gmail.com>
2022-04-29 16:57:17 +0200

    removed livetracking as a module

--- modules/export/export-modules.ts
+++ modules/export/export-modules.ts
@@ -1,11 +1,9 @@
 import { BanTracking } from '../ban-tracking.js'
 import { ColorTracking } from '../color-tracking.js'
-import { LiveTracking } from '../live_tracking/live-tracking.js'
 import { TimeoutTracking } from '../timeout-tracking.js'
 import { Module } from './module.js'
-const liveTracking = new LiveTracking()
 const timeoutModule = new TimeoutTracking()
 const colorModule = new ColorTracking()
 const banModule = new BanTracking()
-const modules: Module[] = [timeoutModule, banModule, colorModule, liveTracking]
+const modules: Module[] = [timeoutModule, banModule, colorModule]
 export { modules }
