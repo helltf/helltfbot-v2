284279d3107af750d70d13bc35fdd38da5b86c66
helltf <Dario.Zachow5530@gmail.com>
2022-04-29 15:00:00 +0200

    formatting

--- modules/live_tracking/types.ts
+++ modules/live_tracking/types.ts
@@ -1,5 +1,13 @@
 import ReconnectingWebSocket from 'reconnecting-websocket'
 import { Notification } from '../../db/export-entities.js'
+
+export type PubSubMessageEventType =
+	| 'stream-up'
+	| 'stream-down'
+	| 'broadcast_settings_update'
+
+export type PubSubType = 'RESPONSE' | 'MESSAGE' | 'PONG' | 'LISTEN'
+
 export interface PubSubData<T extends PubSubDataMessage> {
 	topic: string
 	message: T
@@ -8,12 +16,14 @@ export interface PubSubData<T extends PubSubDataMessage> {
 interface PubSubDataMessage {
 	type: PubSubMessageEventType
 }
-export interface NotificationMessageInfo{
+
+export interface NotificationMessageInfo {
 	type: UpdateEventType
-	message: string, 
-	streamer: string,
+	message: string
+	streamer: string
 	notifiedUsers: Notification[]
 }
+
 export interface StatusMessage extends PubSubDataMessage {
 	server_time: number
 	play_delay: number
@@ -28,20 +38,6 @@ export interface SettingMessage extends PubSubDataMessage {
 	game?: string
 	old_game_id?: number
 	game_id?: number
-}
-
-export type PubSubMessageEventType =
-	| 'stream-up'
-	| 'stream-down'
-	| 'broadcast_settings_update'
-
-export type PubSubType = 'RESPONSE' | 'MESSAGE' | 'PONG' | 'LISTEN'
-
-export enum UpdateEventType {
-	LIVE = 'live',
-	OFFLINE = 'offline',
-	TITLE = 'title',
-	GAME = 'game',
 }
 
 export interface WebSocketConnection {
@@ -70,4 +66,11 @@ export interface TwitchNotificationMessage {
 export enum TopicType {
 	INFO = 'broadcast-settings-update.',
 	LIVE = 'video-playback-by-id.',
+}
+
+export enum UpdateEventType {
+	LIVE = 'live',
+	OFFLINE = 'offline',
+	TITLE = 'title',
+	GAME = 'game',
 }
