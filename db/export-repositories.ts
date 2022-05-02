f525e599eab60c34ccdf7f6fa843e6895ff0c0f8
helltf <Dario.Zachow5530@gmail.com>
2022-05-02 18:52:34 +0200

    created class from interface

--- db/export-repositories.ts
+++ db/export-repositories.ts
@@ -1,32 +1,58 @@
 import { Repository } from 'typeorm'
-import { WordleWord, ColorHistory, Channel, WatchChannel, TwitchUser, Ban, Timeout, CommandEntity, Notification, NotificationChannel } from './export-entities.js'
+import {
+	WordleWord,
+	ColorHistory,
+	Channel,
+	WatchChannel,
+	TwitchUser,
+	Ban,
+	Timeout,
+	CommandEntity,
+	Notification,
+	NotificationChannel,
+} from './export-entities.js'
 import { AppDataSource } from './export-orm.js'
-
-
-const repositories = {
-	wordleRepo:  AppDataSource.getRepository(WordleWord),
-	colorRepo:  AppDataSource.getRepository(ColorHistory),
-	channelRepo:  AppDataSource.getRepository(Channel),
-	watchRepo:  AppDataSource.getRepository(WatchChannel),
-    userRepo: AppDataSource.getRepository(TwitchUser),
-	banRepo: AppDataSource.getRepository(Ban),
-	timeoutRepo: AppDataSource.getRepository(Timeout),
-	commandRepo: AppDataSource.getRepository(CommandEntity),
-	notificationRepo: AppDataSource.getRepository(Notification),
-	notificationChannelRepo: AppDataSource.getRepository(NotificationChannel)
-}
 
 export interface DbRepositories {
 	wordleRepo: Repository<WordleWord>
 	colorRepo: Repository<ColorHistory>
 	channelRepo: Repository<Channel>
-    watchRepo: Repository<WatchChannel>
-    userRepo: Repository<TwitchUser>
-	banRepo: Repository<Ban>,
+	watchRepo: Repository<WatchChannel>
+	userRepo: Repository<TwitchUser>
+	banRepo: Repository<Ban>
 	timeoutRepo: Repository<Timeout>
 	commandRepo: Repository<CommandEntity>
 	notificationRepo: Repository<Notification>
 	notificationChannelRepo: Repository<NotificationChannel>
 }
 
-export { repositories }
+export class DB implements DbRepositories {
+	wordleRepo: Repository<WordleWord>
+	colorRepo: Repository<ColorHistory>
+	channelRepo: Repository<Channel>
+	watchRepo: Repository<WatchChannel>
+	userRepo: Repository<TwitchUser>
+	banRepo: Repository<Ban>
+	timeoutRepo: Repository<Timeout>
+	commandRepo: Repository<CommandEntity>
+	notificationRepo: Repository<Notification>
+	notificationChannelRepo: Repository<NotificationChannel>
+
+	constructor() {
+		this.wordleRepo = AppDataSource.getRepository(WordleWord)
+		this.colorRepo = AppDataSource.getRepository(ColorHistory)
+		this.channelRepo = AppDataSource.getRepository(Channel)
+		this.watchRepo = AppDataSource.getRepository(WatchChannel)
+		this.userRepo = AppDataSource.getRepository(TwitchUser)
+		this.banRepo = AppDataSource.getRepository(Ban)
+		this.timeoutRepo = AppDataSource.getRepository(Timeout)
+		this.commandRepo = AppDataSource.getRepository(CommandEntity)
+		this.notificationRepo = AppDataSource.getRepository(Notification)
+		this.notificationChannelRepo =
+			AppDataSource.getRepository(NotificationChannel)
+	}
+	async initialize(): Promise<DB>{
+		await AppDataSource.initialize()
+		return this
+	}
+}
