564f8f6d6d5eb25b25aeb614eeb3480b5305bb5a
helltf <Dario.Zachow5530@gmail.com>
2022-05-03 12:56:59 +0200

    removed testing

--- helltfbot.ts
+++ helltfbot.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config'
-import { DataSource } from 'typeorm'
 import { TwitchBot } from './client/bot.js'
 import { mainClient } from './client/main-bot.js'
 import { watchClient } from './client/track-bot.js'
@@ -11,7 +10,6 @@ globalThis.hb = new TwitchBot(mainClient, watchClient)
 	hb.setCommands(commands)
 	hb.setRepositories(await new DB().initialize())
 	await hb.init()
-	console.log(await hb.db.userRepo.find())
 	hb.joinChannels()
 	hb.startJobs()
 	hb.initModules()
