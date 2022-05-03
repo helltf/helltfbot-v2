312fd28ac2700989c18c326521f408c5d7b28200
helltf <Dario.Zachow5530@gmail.com>
2022-05-03 17:15:27 +0200

    removed logging of clearing

--- test/test-utils/clear.ts
+++ test/test-utils/clear.ts
@@ -1,10 +1,8 @@
 import { DataSource } from "typeorm";
 
 const clearDb =  async(dataSource: DataSource) => {
-    console.log('cleared db')
     await dataSource.dropDatabase()
     await dataSource.synchronize()
-    console.log('cleared db')
 }
 
 export {clearDb}
