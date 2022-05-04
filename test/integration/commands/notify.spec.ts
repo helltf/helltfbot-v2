3f405865164be29967ddb26ebc9ef605068d96b1
helltf <Dario.Zachow5530@gmail.com>
2022-05-04 18:25:59 +0200

    formatting

--- test/integration/commands/notify.spec.ts
+++ test/integration/commands/notify.spec.ts
@@ -1,26 +1,28 @@
-import { DataSource } from "typeorm"
-import { TwitchBot } from "../../../client/bot.js"
-import { mainClient } from "../../../client/main-bot.js"
-import { watchClient } from "../../../client/track-bot.js"
-import { DB } from "../../../db/export-repositories.js"
-import { getOrmConf } from "../../../ormconfig.js"
-import { clearDb } from "../../test-utils/clear.js"
+import 'dotenv/config'
+import { DataSource } from 'typeorm'
+import { TwitchBot } from '../../../client/bot.js'
+import { mainClient } from '../../../client/main-bot.js'
+import { watchClient } from '../../../client/track-bot.js'
+import { DB } from '../../../db/export-repositories.js'
+import { getOrmConf } from '../../../ormconfig.js'
+import { clearDb } from '../../test-utils/clear.js'
 
 describe('', () => {
-    globalThis.hb = new TwitchBot(mainClient, watchClient)
+	globalThis.hb = new TwitchBot(mainClient, watchClient)
 
-    beforeAll(async()=>{
-        let src = new DataSource(getOrmConf())
-        hb.db = await new DB(src).initialize()
-    })
+	beforeAll(async () => {
+		let src = new DataSource(getOrmConf())
+		hb.db = await new DB(src).initialize()
+	})
 
-    beforeEach(async()=>{
-        await clearDb(hb.db.dataSource)
-    })
+	beforeEach(async () => {
+		await clearDb(hb.db.dataSource)
+	})
 
-    afterAll(async()=>{
-        await clearDb(hb.db.dataSource)
-    })
+	afterAll(async () => {
+		await clearDb(hb.db.dataSource)
+	})
 
-    it('', ()=> {})
+	it('', () => {
+	})
 })
