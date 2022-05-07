74192ea2e3fef63114681ba6e969c91fad55d3a4
helltf <Dario.Zachow5530@gmail.com>
2022-05-07 19:18:47 +0200

    adding tests for suggestion command

---
+++ test/integration/commands/suggest.spec.ts
@@ -0,0 +1,44 @@
+import { suggest } from "../../../commands/cmd/suggest.js"
+import { getExampleUserState } from "../../../spec/examples/user.js"
+import { clearDb } from "../../test-utils/clear.js"
+import { setupDatabase } from "../../test-utils/setup-db.js"
+
+describe('test suggest command', () => {
+    let channel
+    let user = getExampleUserState()
+    
+    beforeAll(async () => {
+        channel = 'channel'
+        await setupDatabase()
+    })
+
+    
+	afterEach(async () => {
+		await clearDb(hb.db.dataSource)
+	})
+
+    beforeEach(async () => {
+		await clearDb(hb.db.dataSource)
+	})
+
+    it('suggestion is undefined return error', async() => {
+        let suggestion = ''
+        let response = await suggest.execute(channel, user, [suggestion])
+
+        expect(response.success).toBeFalse()
+    })
+
+    it('suggestion is defined and response is successful', async() => {
+        let message = ['add']
+        let response = await suggest.execute(channel, user, message)
+
+        expect(response.success).toBeTrue()
+    })
+
+    it('one word suggestion is defined and saved into db', async() => {
+        let message = ['add']
+        let response = await suggest.execute(channel, user, message)
+        let savedEntity = await hb.db.suggestionRepo.find()
+        expect(response.success).toBeTrue()
+    })
+})
