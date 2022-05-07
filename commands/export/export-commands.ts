8196a07ab59db3c72786a583fa99e2cebd6c5011
helltf <Dario.Zachow5530@gmail.com>
2022-05-07 19:17:53 +0200

    exporting new command

--- commands/export/export-commands.ts
+++ commands/export/export-commands.ts
@@ -2,7 +2,8 @@ import { ping } from '../cmd/ping.js'
 import { github } from '../cmd/github.js'
 import { website } from '../cmd/website.js'
 import { notify } from '../cmd/notify.js'
+import { suggest } from '../cmd/suggest.js'
 
 
-const commands = [ping, github, website, notify]
+const commands = [ping, github, website, notify, suggest]
 export default commands
