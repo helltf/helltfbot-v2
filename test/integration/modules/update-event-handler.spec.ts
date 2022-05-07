f333c051658a1c651d919401bab7ce1382beca0b
helltf <Dario.Zachow5530@gmail.com>
2022-05-07 12:37:16 +0200

    adding two test suits for event handler

--- test/integration/modules/update-event-handler.spec.ts
+++ test/integration/modules/update-event-handler.spec.ts
@@ -1,12 +1,19 @@
 import 'dotenv/config'
+import { UpdateEventType } from '../../../modules/pubsub/types.js'
 import { UpdateEventHandler } from '../../../modules/pubsub/update-event-handler.js'
+import { getExampleNotificationEntity } from '../../../spec/examples/user.js'
 import { clearDb } from '../../test-utils/clear.js'
 import { setupDatabase } from '../../test-utils/setup-db.js'
+import { Notification } from '../../../db/entity/notification.js'
 
 describe('Test event handler to return the correct messages', () => {
 	let eventHandler: UpdateEventHandler = new UpdateEventHandler()
+    let streamer: string
+    let notification: Notification
 
 	beforeEach(async () => {
+        streamer = `streamer`
+        notification = await getExampleNotificationEntity()
 		eventHandler = new UpdateEventHandler()
 		await clearDb(hb.db.dataSource)
 	})
@@ -15,7 +22,26 @@ describe('Test event handler to return the correct messages', () => {
 		await setupDatabase()
 	})
 
-	it('connect to db', () => {
-        expect('a').toBe('a')
+	it('get notified user no user existing return empty array', async() => {
+        let type = UpdateEventType.LIVE
+
+        let result = await eventHandler.getNotifiedUsers(streamer, type)
+        let expectedResult = []
+
+        expect(result).toEqual(expectedResult)
+    })
+
+    it('get notified user user has notification return 1 notification', async() => {
+        let type = UpdateEventType.LIVE
+        notification[type] = true
+
+        await hb.db.userRepo.save(notification.user)
+        await hb.db.notificationRepo.save(notification)
+
+        let result = await eventHandler.getNotifiedUsers(streamer, type)
+
+        let expectedResult = 1
+
+        expect(result).toHaveSize(expectedResult)
     })
 })
