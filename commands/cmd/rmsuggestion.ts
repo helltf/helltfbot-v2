bf458af82b3b9b31b5c761883a977df051d88458
helltf <Dario.Zachow5530@gmail.com>
2022-05-08 14:12:27 +0200

    implementing deletion of suggestions

--- commands/cmd/rmsuggestion.ts
+++ commands/cmd/rmsuggestion.ts
@@ -1,6 +1,7 @@
 import { Command } from '../export/types.js'
 import { ChatUserstate } from 'tmi.js'
 import { BotResponse } from '../../client/response.js'
+import { Suggestion } from '../../db/export-entities.js'
 
 const rmsuggest = new Command({
 	name: 'rmsuggest',
@@ -20,17 +21,51 @@ const rmsuggest = new Command({
 				response: 'You need to specify an id to delete your suggestion',
 				channel: channel,
 			}
-        if(await idIsValidForUser(parseInt(user['user-id']), parseInt(id)))
+		if (await idIsNotValidForUser(user['user-id'], id))
+			return {
+				success: false,
+				response: `Id ${id} not existing or the suggestion is created by somebody else`,
+				channel: channel,
+			}
+        
+        await deleteSuggestion(id)
+
         return {
-            success: false,
-            response: `Id ${id} not existing or the suggestion is created by somebody else`,
-            channel: channel
+            response: `Succesfully removed your suggestion with id ${id}`,
+            channel: channel,
+            success: true
         }
 	},
 })
 
-async function idIsValidForUser(userId: number, suggestionId:number): Promise<boolean>{
-    return
+async function idIsNotValidForUser(
+	userId: string,
+	suggestionId: string
+): Promise<boolean> {
+	let parsedUserId = parseInt(userId)
+	let parsedSuggestionId = parseInt(suggestionId)
+
+    let entity = await hb.db.suggestionRepo.findOne({
+        where:{
+            id: parsedSuggestionId,
+            user: {
+                id: parsedUserId
+            }
+        },
+        relations: {
+            user: true
+        }
+    }
+    )
+	return entity === null
+}
+
+async function deleteSuggestion(suggestionId: string) {
+    let parsedSuggestionId = parseInt(suggestionId)
+
+    await hb.db.suggestionRepo.delete({
+        id: parsedSuggestionId
+    })
 }
 
 export { rmsuggest }
