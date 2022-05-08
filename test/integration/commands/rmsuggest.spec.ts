87ccc62e8564a8d80e1b587438d7c8584b68bee4
helltf <Dario.Zachow5530@gmail.com>
2022-05-08 14:12:34 +0200

    formatting/ creating last test suit if user differs

--- test/integration/commands/rmsuggest.spec.ts
+++ test/integration/commands/rmsuggest.spec.ts
@@ -1,69 +1,105 @@
-import { ChatUserstate } from "tmi.js"
-import { rmsuggest } from "../../../commands/cmd/rmsuggestion.js"
-import { getExampleUserState } from "../../../spec/examples/user.js"
-import { clearDb } from "../../test-utils/clear.js"
-import { disconnectDatabase } from "../../test-utils/disconnect.js"
-import { saveUserStateAsUser } from "../../test-utils/save-user.js"
-import { setupDatabase } from "../../test-utils/setup-db.js"
+import { ChatUserstate } from 'tmi.js'
+import { rmsuggest } from '../../../commands/cmd/rmsuggestion.js'
+import { getExampleUserState } from '../../../spec/examples/user.js'
+import { clearDb } from '../../test-utils/clear.js'
+import { disconnectDatabase } from '../../test-utils/disconnect.js'
+import { saveUserStateAsUser } from '../../test-utils/save-user.js'
+import { setupDatabase } from '../../test-utils/setup-db.js'
 
-fdescribe('test rmsuggest command', () => {
-    let channel: string
-    let user: ChatUserstate
-    beforeAll(async () => {
-        await setupDatabase()
-    })
-    
-    beforeEach(async () => {
-        channel = 'channel'
-        user = getExampleUserState()
-		await clearDb(hb.db.dataSource)
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
+describe('test rmsuggest command', () => {
+	let channel: string
+	let user: ChatUserstate
+	beforeAll(async () => {
+		await setupDatabase()
 	})
 
-    afterAll(async () => {
-        await disconnectDatabase()
-    })
+	beforeEach(async () => {
+		channel = 'channel'
+		user = getExampleUserState()
+		await clearDb(hb.db.dataSource)
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
+	})
 
-    it('required id is undefined return error message', async() => {
-        let message = ['']
+	afterAll(async () => {
+		await disconnectDatabase()
+	})
 
-        let response = await rmsuggest.execute(channel, user, message)
+	it('required id is undefined return error message', async () => {
+		let message = ['']
 
-        expect(response.success).toBeFalse()
-        expect(response.response).toBe('You need to specify an id to delete your suggestion')
-        expect(response.channel).toBe(channel)
-    })
+		let response = await rmsuggest.execute(channel, user, message)
 
-    it('id is defined but not in database return error', async() => {
-        let id = '1'
-        let message = [id]
+		expect(response.success).toBeFalse()
+		expect(response.response).toBe(
+			'You need to specify an id to delete your suggestion'
+		)
+		expect(response.channel).toBe(channel)
+	})
 
-        let response = await rmsuggest.execute(channel, user, message)
+	it('id is defined but not in database return error', async () => {
+		let id = '1'
+		let message = [id]
 
-        expect(response.success).toBeFalse()
-        expect(response.response).toBe(`Id ${id} not existing or the suggestion is created by somebody else`)
-    })
+		let response = await rmsuggest.execute(channel, user, message)
 
-    it('id is defined and existing in the database, delete the entry',async () => {
-        await saveUserStateAsUser(user)
+		expect(response.success).toBeFalse()
+		expect(response.response).toBe(
+			`Id ${id} not existing or the suggestion is created by somebody else`
+		)
+	})
+
+	it('id is defined and existing in the database, delete the entry', async () => {
+		await saveUserStateAsUser(user)
+
+		let savedEntity = await hb.db.suggestionRepo.save({
+			date: 1,
+			user: {
+				id: parseInt(user['user-id']),
+			},
+			suggestion: 'a',
+		})
+
+		let id = savedEntity.id
+		let message = [`${id}`]
+
+		let response = await rmsuggest.execute(channel, user, message)
+
+		let entity = await hb.db.suggestionRepo.findOneBy({
+			id: id,
+		})
+
+		expect(response.success).toBeTrue()
+		expect(response.response).toBe(
+			`Succesfully removed your suggestion with id ${id}`
+		)
+		expect(entity).toBeNull()
+	})
+
+	it('id is defined but user differs from db entry return fail', async () => {
+		let id = '1'
+		let message = [id]
+		await saveUserStateAsUser(user)
 
         let savedEntity = await hb.db.suggestionRepo.save({
             date: 1,
             user: {
-                id: parseInt(user["user-id"])
+                id: parseInt(user['user-id'])
             },
             suggestion: 'a'
         })
 
-        let id = savedEntity.id
-        let message = [`${id}`]
+		user['user-id'] = '5'
 
-        let response = await rmsuggest.execute(channel, user, message)
-        let entity = await hb.db.suggestionRepo.findOneBy({
-            id: id
+		let response = await rmsuggest.execute(channel, user, message)
+
+        let remainingEntity = await hb.db.suggestionRepo.findOneBy({
+            id: savedEntity.id
         })
-        expect(response.success).toBeTrue()
-        expect(response.response).toBe(`Succesfully removed your suggestion with id ${id}`)
-        expect(entity).toBeNull()
-    })
+
+		expect(response.success).toBeFalse()
+		expect(response.response).toBe(
+			`Id ${id} not existing or the suggestion is created by somebody else`
+		)
+        expect(remainingEntity).toBeDefined()
+	})
 })
