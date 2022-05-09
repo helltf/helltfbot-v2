c996e35fc816125d9ce0d74c86c5268a16b6d5eb
helltf <Dario.Zachow5530@gmail.com>
2022-05-09 21:26:17 +0200

    revoking var to avoid pipeline error

--- helltfbot.ts
+++ helltfbot.ts
@@ -3,7 +3,6 @@ import { TwitchBot } from './client/bot.js'
 import { mainClient } from './client/main-bot.js'
 import { watchClient } from './client/track-bot.js'
 
-var a = 'a'
 globalThis.hb = new TwitchBot(mainClient, watchClient)
 ;(async () => {
   await hb.init()
