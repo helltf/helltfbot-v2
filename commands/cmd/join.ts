d4628d9397b1b192f976ce7fac703034a790dc6b
helltf <Dario.Zachow5530@gmail.com>
2022-05-10 13:20:25 +0200

    sending error response for invalid input

--- commands/cmd/join.ts
+++ commands/cmd/join.ts
@@ -14,14 +14,21 @@ export const join = new Command({
     user: ChatUserstate,
     [joinChannel]: string[]
   ): Promise<BotResponse> => {
-    if (!joinChannel)
-      return {
-        success: false,
-        channel: channel,
-        response: 'channel has to be defined'
-      }
+    const errorResponse: BotResponse = {
+      channel: channel,
+      response: '',
+      success: false
+    }
 
-    if (await isAlreadyConnected(channel)) return
+    if (!joinChannel) {
+      errorResponse.response = 'channel has to be defined'
+      return errorResponse
+    }
+
+    if (await isAlreadyConnected(channel)) {
+      errorResponse.response = 'Already connected to that channel'
+      return errorResponse
+    }
 
     const success = await connectToChannel(channel)
   }
@@ -36,7 +43,7 @@ async function connectToChannel(channel: string): Promise<boolean> {
   }
 }
 
-async function isAlreadyConnected(channel: string) {
+export async function isAlreadyConnected(channel: string): Promise<number> {
   return hb.db.channelRepo.countBy({
     joined: true,
     channel: channel
