ccf8f280bf667604718b3bf6e1ce86fc4e7abd2b
helltf <Dario.Zachow5530@gmail.com>
2022-05-14 11:20:31 +0200

    refactoring function to match tests

--- utilities/twitch/permission.ts
+++ utilities/twitch/permission.ts
@@ -1,11 +1,11 @@
 import { Userstate, Badges } from 'tmi.js'
 
 const getChatPermissions = (badges: Badges): number => {
-  if (badges.broadcaster !== undefined) return 1
-  if (badges.moderator !== undefined) return 2
-  if (badges.vip !== undefined) return 3
-  if (badges.subscriber !== undefined || badges.founder !== undefined) return 4
-  return 5
+  if (badges.broadcaster !== undefined) return 4
+  if (badges.moderator !== undefined) return 3
+  if (badges.vip !== undefined) return 2
+  if (badges.subscriber !== undefined || badges.founder !== undefined) return 1
+  return 0
 }
 
 const returnLowerPermsissions = (db: number, chat: number) => {
