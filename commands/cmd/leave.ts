9cbe6f5d96683891021f09bfc3ecb56f14cd7593
helltf <Dario.Zachow5530@gmail.com>
2022-05-15 12:46:24 +0200

    updating channels in db after leaving

--- commands/cmd/leave.ts
+++ commands/cmd/leave.ts
@@ -13,17 +13,25 @@ export const leave = new Command({
   execute: async (
     channel: string,
     user: ChatUserstate,
-    [leaveChannel]: string[]
+    [channeltoLeave]: string[]
   ): Promise<BotResponse> => {
     const errorMessage: BotResponse = { channel, success: false, response: '' }
-    if (!leaveChannel) {
+    if (!channeltoLeave) {
       errorMessage.response = 'You need to define a channel'
       return errorMessage
     }
 
-    if (await isNotConnectedToChannel(leaveChannel)) {
+    if (await isNotConnectedToChannel(channeltoLeave)) {
       errorMessage.response = 'Not connected to channel'
       return errorMessage
+    }
+    const { success, message } = await leaveChannel(channeltoLeave)
+
+    if (success) await updateChannelProperty(channeltoLeave)
+    return {
+      success,
+      response: message,
+      channel: channel
     }
   }
 })
@@ -35,4 +43,33 @@ async function isNotConnectedToChannel(channel: string): Promise<boolean> {
       joined: true
     })) === 0
   )
+}
+
+export async function updateChannelProperty(channel: string) {
+  await hb.db.channelRepo.update(
+    {
+      channel: channel
+    },
+    {
+      joined: false
+    }
+  )
+}
+
+export async function leaveChannel(channel: string): Promise<{
+  success: boolean
+  message: string
+}> {
+  try {
+    await hb.client.join(channel)
+    return {
+      success: true,
+      message: 'Successfully left the channel'
+    }
+  } catch (e) {
+    return {
+      success: false,
+      message: 'Could not join the channel'
+    }
+  }
 }
