b3c18b9239a8116c5302140ecdd33e20786c40cb
helltf <Dario.Zachow5530@gmail.com>
2022-05-19 20:46:27 +0200

    fix: changing access of token

--- api/twitch/autorization.ts
+++ api/twitch/autorization.ts
@@ -3,7 +3,7 @@ import { Headers, HeadersInit } from 'node-fetch'
 const getAuthorizationHeader = (): HeadersInit => {
   const headers = new Headers()
 
-  headers.set('Authorization', 'Bearer ' + hb.twitchAT)
+  headers.set('Authorization', 'Bearer ' + hb.api.twitch.accessToken)
   headers.set('Client-Id', process.env.CLIENT_ID)
 
   return headers
