5f84369cc7c616656c1ddcb055d6c9911b00bcd5
helltf <Dario.Zachow5530@gmail.com>
2022-05-19 20:17:54 +0200

    feat: new Api type to contain all apis

--- client/types.ts
+++ client/types.ts
@@ -1,6 +1,10 @@
 import { ChatUserstate } from 'tmi.js'
+import { TwitchApi } from '../api/twitch/export-api.js'
 import { PermissionLevel } from '../utilities/twitch/types.js'
 
 export interface TwitchUserState extends ChatUserstate {
   permission?: PermissionLevel
+}
+export interface APIs {
+  twitch?: TwitchApi
 }
