226933513be042f5b3a470c01d5b805a98714962
helltf <Dario.Zachow5530@gmail.com>
2022-05-25 17:48:23 +0200

    fix: saving of colors

--- src/modules/color-tracking.ts
+++ src/modules/color-tracking.ts
@@ -2,56 +2,69 @@ import { ChatUserstate } from 'tmi.js'
 import { Module } from './export/module.js'
 
 export class ColorTracking implements Module {
-  name = 'Color'
-  MAX_SAVED_COLORS = 15
+  name: string = 'Color'
+  MAX_SAVED_COLORS: number = 15
+
   async initialize() {
-    hb.client.on(
-      'chat',
-      async (
-        _: string,
-        { 'user-id': userId, color: userColor }: ChatUserstate
-      ) => {
-        const savedColors = (
-          await hb.db.userRepo.findOne({
-            where: { id: Number(userId) },
-            relations: {
-              colors: true
-            }
-          })
-        )?.colors?.history
-
-        if (!savedColors) {
-          hb.db.colorRepo.save({
-            twitch_id: Number(userId),
-            history: [userColor],
-            change_timestamp: Date.now(),
-            register_timestamp: Date.now()
-          })
-          return
-        }
-
-        const latestColor = savedColors[savedColors.length - 1]
-
-        if (latestColor !== userColor) {
-          const updatedColors = this.updateCurrentColors(savedColors, userColor)
-
-          hb.db.colorRepo.save({
-            twitch_id: Number(userId),
-            history: updatedColors,
-            change_timestamp: Date.now()
-          })
-        }
-      }
-    )
+    hb.client.on('chat', (c: string, u: ChatUserstate) => {
+      this.handleColorChanges(c, u)
+    })
   }
 
-  async handleColorChanges() {}
+  async handleColorChanges(
+    _: string,
+    { 'user-id': userId, color: userColor }: ChatUserstate
+  ) {
+    if (!userId) return
+    const id = Number(userId)
+
+    const savedColors = (
+      await hb.db.userRepo.findOne({
+        where: { id: id },
+        relations: {
+          colors: true
+        }
+      })
+    )?.colors?.history
+
+    if (!savedColors) {
+      hb.db.colorRepo.save({
+        user: {
+          id: id
+        },
+        history: [userColor],
+        change_timestamp: Date.now(),
+        register_timestamp: Date.now()
+      })
+      return
+    }
+
+    const latestColor = savedColors[savedColors.length - 1]
+
+    if (latestColor !== userColor) {
+      const updatedColors = this.updateCurrentColors(savedColors, userColor)
+
+      hb.db.colorRepo.update(
+        {
+          user: {
+            id: id
+          }
+        },
+        {
+          history: updatedColors,
+          change_timestamp: Date.now()
+        }
+      )
+    }
+  }
+
   setNewPosition(colors: string[], newColor: string): string[] {
     const index = colors.findIndex((c) => c === newColor)
     colors.splice(index, 1)
     colors.push(newColor)
     return colors
   }
+
   addNewColor(
     colors: string[],
     newColor: string,
@@ -66,6 +79,7 @@ export class ColorTracking implements Module {
 
     return colors
   }
+
   updateCurrentColors(colors: string[], newColor: string): string[] {
     if (colors.includes(newColor)) {
       return this.setNewPosition(colors, newColor)
