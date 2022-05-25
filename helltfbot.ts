787502508e8bf5501971cf0d4e123cc9531f69ca
helltf <Dario.Zachow5530@gmail.com>
2022-05-25 18:08:51 +0200

    feat: removing old join function

--- helltfbot.ts
+++ helltfbot.ts
@@ -1,11 +1,10 @@
 import 'dotenv/config'
 import { TwitchBot } from './src/client/bot.js'
-import { mainClient } from './src/client/main-bot.js'
+import { client } from './src/client/main-bot.js'
 
-globalThis.hb = new TwitchBot(mainClient)
+globalThis.hb = new TwitchBot(client)
 ;(async () => {
   await hb.init()
-  hb.joinChannels()
   hb.startJobs()
   hb.initModules()
 })()
