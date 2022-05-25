44721b4d0781d8cb225db6e1261e4d8481035930
helltf <Dario.Zachow5530@gmail.com>
2022-05-25 18:09:21 +0200

    refactoring: renaming mainclient to client

--- test/test-utils/setup-db.ts
+++ test/test-utils/setup-db.ts
@@ -2,11 +2,11 @@ import 'dotenv/config'
 import { DataSource } from 'typeorm'
 import { getOrmConf } from '../../ormconfig.js'
 import { TwitchBot } from '../../src/client/bot.js'
-import { mainClient } from '../../src/client/main-bot.js'
+import { client } from '../../src/client/main-bot.js'
 import { DB } from '../../src/db/export-repositories.js'
 
 const setupDatabase = async () => {
-  globalThis.hb = new TwitchBot(mainClient)
+  globalThis.hb = new TwitchBot(client)
   const src = new DataSource(getOrmConf())
   hb.db = await new DB(src).initialize()
 }
