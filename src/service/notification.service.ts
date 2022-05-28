ff7ee610e44aee7112a718eb67cdfd80e518cf95
helltf <dario.zachow5530@gmail.com>
2022-05-28 15:40:24 +0200

    test: check for existing notifications

--- src/service/notification.service.ts
+++ src/service/notification.service.ts
@@ -1,5 +1,35 @@
+import { StreamerEventType } from "../commands/cmd/notify.js"
+import { UpdateEventType } from "../modules/pubsub/types.js"
+
 export class NotificationService {
-    async cleanNotifications() {
+    async cleanAllNotifications() {
+        const notificationChannels = await hb.db.notificationChannelRepo.find()
+
+        for (const { name, status, setting } of notificationChannels) {
+            if (setting) this.cleanNotificationsForStreamer(name, StreamerEventType.SETTING)
+            if (status) this.cleanNotificationsForStreamer(name, StreamerEventType.STATUS)
+        }
+    }
+
+    async cleanNotificationsForStreamer(streamer: string, event: StreamerEventType) {
+        const updateEvents = this.mapEventTypeToUpdateType(event)
+
+        const efxistingNotification = await this.isNotificationExisting(streamer, updateEvents)
+
+    }
+
+    async isNotificationExisting(streamer: string, updateEvents: UpdateEventType[]): Promise<boolean> {
+        const queryParams = updateEvents.map(e => { return { streamer, [e]: true } })
+
+        return (await hb.db.notificationRepo.find({
+            where: queryParams
+        })).length !== 0
+    }
+
+
+    mapEventTypeToUpdateType(event: StreamerEventType): UpdateEventType[] {
+        if (event === StreamerEventType.SETTING) return [UpdateEventType.GAME, UpdateEventType.TITLE]
+        return [UpdateEventType.OFFLINE, UpdateEventType.LIVE]
 
     }
 }
--- test/integration/service/notification.service.spec.ts
+++ test/integration/service/notification.service.spec.ts
@@ -1,10 +1,14 @@
+import { StreamerEventType } from "../../../src/commands/cmd/notify.js"
+import { UpdateEventType } from "../../../src/modules/pubsub/types.js"
 import { NotificationService } from "../../../src/service/notification.service.js"
 import { clearDb } from "../../test-utils/clear.js"
 import { disconnectDatabase } from "../../test-utils/disconnect.js"
+import { getExampleNotificationEntity } from "../../test-utils/example.js"
+import { saveNotificationWithUser } from "../../test-utils/save-notification.js"
 import { setupDatabase } from "../../test-utils/setup-db.js"
 
 
-describe('test notificatin service', () => {
+fdescribe('test notificatin service', () => {
     let service: NotificationService
 
     beforeAll(async () => {
@@ -21,4 +25,44 @@ describe('test notificatin service', () => {
         await disconnectDatabase()
     })
 
+    it('map setting event return game and title', () => {
+        const streamerEvent = StreamerEventType.SETTING
+
+        const updateEventTypes = service.mapEventTypeToUpdateType(streamerEvent)
+
+        expect(updateEventTypes).toContain(UpdateEventType.GAME)
+        expect(updateEventTypes).toContain(UpdateEventType.TITLE)
+    })
+
+    it('map status event return live and offline', () => {
+        const streamerEvent = StreamerEventType.STATUS
+
+        const updateEventTypes = service.mapEventTypeToUpdateType(streamerEvent)
+
+        expect(updateEventTypes).toContain(UpdateEventType.LIVE)
+        expect(updateEventTypes).toContain(UpdateEventType.OFFLINE)
+    })
+
+    it('no notification existing return false', async () => {
+        const streamer = 'exampleStreamer'
+        const updateEvents = service.mapEventTypeToUpdateType(StreamerEventType.STATUS)
+        const isExisting = await service.isNotificationExisting(streamer, updateEvents)
+
+        expect(isExisting).toBeFalse()
+    })
+    it('notification exists return true', async () => {
+
+        const updateEvents = service.mapEventTypeToUpdateType(StreamerEventType.STATUS)
+
+        const notification = getExampleNotificationEntity({
+            [updateEvents[0]]: true
+        })
+
+        await saveNotificationWithUser(notification)
+
+        const isExisting = await service.isNotificationExisting(notification.streamer, updateEvents)
+
+        expect(isExisting).toBeTrue()
+
+    })
 })
