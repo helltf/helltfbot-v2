7a4ac9fcf2287c7b3c8d4549d8d55ee49ac8c4b3
helltf <Dario.Zachow5530@gmail.com>
2022-05-31 14:46:23 +0200

    fix: no longer spawning infinite semicolons

--- helltfbot.ts
+++ helltfbot.ts
@@ -2,9 +2,11 @@ import 'dotenv/config'
 import { TwitchBot } from './src/client/bot.js'
 import { client } from './src/client/main-client.js'
 
-globalThis.hb = new TwitchBot(client)
-  ; (async () => {
+globalThis.hb = new TwitchBot(client);
+{
+  ;(async () => {
     await hb.init()
     hb.startJobs()
     hb.initModules()
   })()
+}
