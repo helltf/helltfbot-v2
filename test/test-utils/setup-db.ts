12c0706bb537e1ede4060ca72b137e5ad424a6f0
helltf <Dario.Zachow5530@gmail.com>
2022-06-01 08:03:56 +0200

    feat: no longer using client as a constructor param

--- test/test-utils/setup-db.ts
+++ test/test-utils/setup-db.ts
@@ -1,11 +1,10 @@
 import 'dotenv/config'
 import { getOrmConf } from '../../ormconfig.js'
 import { TwitchBot } from '../../src/client/bot.js'
-import { client } from '../../src/client/main-client.js'
 import { DB } from '../../src/db/export-repositories.js'
 
 const setupDatabase = async () => {
-  globalThis.hb = new TwitchBot(client)
+  globalThis.hb = new TwitchBot()
   hb.db = await new DB(getOrmConf()).initialize()
 }
 
