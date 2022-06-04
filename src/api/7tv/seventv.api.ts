b27038c0c6299c40b3f63c3b965726a6557b1bc2
helltf <Dario.Zachow5530@gmail.com>
2022-06-04 11:12:59 +0200

    feat: fetching 7tv emotes

--- src/api/7tv/seventv.api.ts
+++ src/api/7tv/seventv.api.ts
@@ -1,1 +1,31 @@
-export class SevenTvApi {}
+import { Resource, ResourceError, ResourceSuccess } from "../types.js"
+
+export class SevenTvApi {
+  url = 'https://api.7tv.app/v2/users/'
+
+  async fetchEmotes(channel: string): Promise<Resource<string[]>> {
+    const error = new ResourceError('Error fetching 7tv emotes')
+
+    try {
+      const emotes = (await (
+        await fetch(this.url + channel + '/emotes')
+      ).json()) as SeventvEmoteResponse[]
+      return new ResourceSuccess(emotes.map((e) => e.name))
+    } catch (e) {
+      return error
+    }
+  }
+
+  async getEmotesForChannel(channel: string): Promise<string[]> {
+    const emotes = await this.fetchEmotes(channel)
+
+    if (emotes instanceof ResourceError) return []
+
+    return emotes.data
+  }
+}
+
+interface SeventvEmoteResponse {
+  id: string
+  name: string
+}
