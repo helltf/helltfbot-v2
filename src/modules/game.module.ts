35461f629dc782829cfda3c6ad26d8ecb14686e8
helltf <Dario.Zachow5530@gmail.com>
2022-06-05 21:20:07 +0200

    feat:  sending proper message after event

--- src/modules/game.module.ts
+++ src/modules/game.module.ts
@@ -1,5 +1,6 @@
 import { ChatUserstate } from 'tmi.js'
 import { Emotegame } from '../games/emotegame.js'
+import { EmoteGameInputResult } from '../games/types.js'
 import { Module } from './export/module.js'
 
 export class GameModule implements Module {
@@ -15,5 +16,22 @@ export class GameModule implements Module {
 
   input(channel: string, user: ChatUserstate, message: string) {
     const game = hb.games.getGame(channel)
+
+    if (!game) return
+
+    const result = game.input(message)
+
+    if (result === EmoteGameInputResult.FINISHED) {
+      return hb.sendMessage(
+        `${user.username} has guessed the emote. The emote was ${game.emote}`
+      )
+    }
+
+    if (result === EmoteGameInputResult.LETTER_CORRECT) {
+      return hb.sendMessage(`
+        ${
+          user.username
+        } has guessed the letter ${message}. The missing letters are ${game.getLetterString()}`)
+    }
   }
 }
