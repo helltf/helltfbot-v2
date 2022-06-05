58a3dfa75bef786e5bffc214751ac29ea1a837ce
helltf <Dario.Zachow5530@gmail.com>
2022-06-05 18:07:51 +0200

    test: test return values from input function

--- test/unit/games/emotegame.spec.ts
+++ test/unit/games/emotegame.spec.ts
@@ -1,4 +1,5 @@
 import { Emotegame } from "../../../src/games/emotegame.js"
+import { EmoteGameInputResult } from "../../../src/games/types.js"
 
 describe('test emotegame class', () => {
   let game: Emotegame
@@ -21,5 +22,51 @@ describe('test emotegame class', () => {
         expect(underscores).toEqual(expectedResult)
       })
     }
+  })
+
+  it('guessed letters for emote should have 5 underscores', () => {
+    const expectedArray = Array(game.emote.length).fill('_')
+    expect(game.guessedLetters).toEqual(expectedArray)
+  })
+
+  fdescribe('input function', () => {
+    it('input is empty string return nothing', () => {
+      const input = ''
+      const state = game.input(input)
+
+      expect(state).toBe(EmoteGameInputResult.NOTHING)
+    })
+
+    it('input is longer than 1 return nothing', () => {
+      const input = 'abc'
+
+      const state = game.input(input)
+
+      expect(state).toBe(EmoteGameInputResult.NOTHING)
+    })
+
+    it('input is exactly the emote return finished', () => {
+      const input = game.emote
+
+      const state = game.input(input)
+
+      expect(state).toBe(EmoteGameInputResult.FINISHED)
+    })
+
+    it('input letter is part of emote return letter correct', () => {
+      const input = 'e'
+
+      const state = game.input(input)
+
+      expect(state).toBe(EmoteGameInputResult.LETTER_CORRECT)
+    })
+
+    it('input letter is not part of emote return nothing', () => {
+      const input = 'a'
+
+      const state = game.input(input)
+
+      expect(state).toBe(EmoteGameInputResult.NOTHING)
+    })
   })
 })
