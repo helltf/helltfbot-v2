96fb5975e2fd7a7d954620f4bcc25fba2d426c2d
helltf <Dario.Zachow5530@gmail.com>
2022-06-06 00:45:48 +0200

    fix: correcting random function

--- src/utilities/random.ts
+++ src/utilities/random.ts
@@ -1,3 +1,3 @@
-export const random = (lowerLimit = 0, upperLimit = 1) => {
-  return Math.floor(Math.random() * upperLimit) + lowerLimit
+export const random = (lowerLimit = 0, upperLimit = 1): number => {
+  return Math.floor(Math.random() * (upperLimit + lowerLimit + 1))
 }
