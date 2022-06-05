b9c0339fec59bdf24ae21cc7a4b762ceae485913
helltf <Dario.Zachow5530@gmail.com>
2022-06-05 19:20:49 +0200

    test: testing input functions and string function

--- test/unit/games/emotegame.spec.ts
+++ test/unit/games/emotegame.spec.ts
@@ -26,47 +26,177 @@ describe('test emotegame class', () => {
 
   it('guessed letters for emote should have 5 underscores', () => {
     const expectedArray = Array(game.emote.length).fill('_')
-    expect(game.guessedLetters).toEqual(expectedArray)
+    expect(game.currentLetters).toEqual(expectedArray)
   })
 
-  fdescribe('input function', () => {
+  describe('input result function', () => {
     it('input is empty string return nothing', () => {
       const input = ''
-      const state = game.input(input)
+      const result = game.getInputResult(input)
 
-      expect(state).toBe(EmoteGameInputResult.NOTHING)
+      expect(result).toBe(EmoteGameInputResult.NOTHING)
     })
 
     it('input is longer than 1 return nothing', () => {
       const input = 'abc'
 
-      const state = game.input(input)
+      const result = game.getInputResult(input)
 
-      expect(state).toBe(EmoteGameInputResult.NOTHING)
+      expect(result).toBe(EmoteGameInputResult.NOTHING)
     })
 
     it('input is exactly the emote return finished', () => {
       const input = game.emote
 
-      const state = game.input(input)
+      const result = game.getInputResult(input)
 
-      expect(state).toBe(EmoteGameInputResult.FINISHED)
+      expect(result).toBe(EmoteGameInputResult.FINISHED)
     })
 
     it('input letter is part of emote return letter correct', () => {
       const input = 'e'
 
-      const state = game.input(input)
+      const result = game.getInputResult(input)
 
-      expect(state).toBe(EmoteGameInputResult.LETTER_CORRECT)
+      expect(result).toBe(EmoteGameInputResult.LETTER_CORRECT)
     })
 
     it('input letter is not part of emote return nothing', () => {
       const input = 'a'
 
-      const state = game.input(input)
+      const result = game.getInputResult(input)
 
-      expect(state).toBe(EmoteGameInputResult.NOTHING)
+      expect(result).toBe(EmoteGameInputResult.NOTHING)
+    })
+
+    it('input letter is part of emote but uppercase return letter correct', () => {
+      const input = 'E'
+
+      const result = game.getInputResult(input)
+
+      expect(result).toBe(EmoteGameInputResult.LETTER_CORRECT)
+    })
+
+    it('input contains two letters of correct word return nothing', () => {
+      const input = 'em'
+
+      const result = game.getInputResult(input)
+
+      expect(result).toBe(EmoteGameInputResult.NOTHING)
+    })
+    it('input is correct but not same case return finished', () => {
+      const input = 'EMOTE'
+
+      const result = game.getInputResult(input)
+
+      expect(result).toBe(EmoteGameInputResult.FINISHED)
+    })
+
+    it('input is correct but has already been written', () => {
+      const input = 'e'
+      game.input(input)
+
+      const result = game.getInputResult(input)
+
+      expect(result).toBe(EmoteGameInputResult.NOTHING)
+    })
+  })
+
+  describe('input function', () => {
+    it("input results in nothing don't change guessed letters", () => {
+      const input = 'a'
+
+      const result = game.input(input)
+
+      const expectedResult = game.generateUnderscores(game.emote)
+
+      expect(game.currentLetters).toEqual(expectedResult)
+      expect(result).toBe(EmoteGameInputResult.NOTHING)
+    })
+
+    it('input is correct letter updating one locations of that letter', () => {
+      const input = 'm'
+
+      game.input(input)
+
+      const expectedResult = game.generateUnderscores(game.emote)
+      expectedResult[1] = input
+
+      expect(game.currentLetters).toEqual(expectedResult)
+    })
+
+    it('input is correct letter updating all locations of that letter', () => {
+      const input = 'e'
+
+      game.input(input)
+
+      const expectedResult = game.generateUnderscores(game.emote)
+      expectedResult[0] = input
+      expectedResult[4] = input
+
+      expect(game.currentLetters).toEqual(expectedResult)
+    })
+
+    it('input is emote return finished', () => {
+      const input = 'emote'
+
+      const result = game.input(input)
+
+      expect(result).toBe(EmoteGameInputResult.FINISHED)
+    })
+
+    it('input e and m updating current letters for both inputs', () => {
+      const input1 = 'e'
+      const input2 = 'm'
+
+      game.input(input1)
+      game.input(input2)
+
+      const expectedResult = game.generateUnderscores(game.emote)
+      expectedResult[0] = input1
+      expectedResult[4] = input1
+      expectedResult[1] = input2
+
+      expect(game.currentLetters).toEqual(expectedResult)
+    })
+  })
+
+  describe('letter string', () => {
+    it('no letter is guessed return only underscores', () => {
+      const result = game.getLetterString()
+
+      const expectedString = Array(emote.length).fill('_').join(' ')
+
+      expect(result).toBe(expectedString)
+    })
+
+    it('letter e has been guessed return underscores but e replaced', () => {
+      const input = 'e'
+      game.input(input)
+
+      const result = game.getLetterString()
+
+      const expectedString = Array(emote.length).fill('_')
+      expectedString[0] = input
+      expectedString[4] = input
+
+      expect(result).toBe(expectedString.join(' '))
+    })
+
+    it('letter e and m has been guessed return underscores with e and m replaced', () => {
+      const inputE = 'e'
+      const inputM = 'm'
+      game.input(inputE)
+      game.input(inputM)
+
+      const result = game.getLetterString()
+
+      const expectedString = Array(emote.length).fill('_')
+      expectedString[0] = inputE
+      expectedString[4] = inputE
+      expectedString[1] = inputM
+
+      expect(result).toBe(expectedString.join(' '))
     })
   })
 })
