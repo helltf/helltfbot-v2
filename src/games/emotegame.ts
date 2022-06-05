6e0098e0040030bf175f034b23df6a34c8f77d29
helltf <Dario.Zachow5530@gmail.com>
2022-06-05 16:15:46 +0200

    feat: generating underscores for emote

--- src/games/emotegame.ts
+++ src/games/emotegame.ts
@@ -1,12 +1,19 @@
+import { Emote } from '../commands/cmd/emotegame.js'
 import { ChatGame } from './types.js'
 
 export class Emotegame implements ChatGame {
   channel: string
   emote: string
+  guessedLetters: string[]
   EXPIRING_AFTER: number = 1000 * 60 * 10
 
   constructor(channel: string, emote: string) {
     this.channel = channel
     this.emote = emote
+    this.guessedLetters = this.generateUnderscores(emote)
+  }
+
+  generateUnderscores(emote: Emote): string[] {
+    return Array(emote.length).fill('_')
   }
 }
