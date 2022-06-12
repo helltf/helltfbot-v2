82b9fb35b84c017fda2d55003ffeb92b5e156793
helltf <Dario.Zachow5530@gmail.com>
2022-06-12 18:06:25 +0200

    feat: adding level command

---
+++ src/commands/cmd/level.ts
@@ -0,0 +1,46 @@
+import { TwitchUserState, BotResponse } from "@src/client/types";
+import { Command } from "@src/commands/types";
+import { PermissionLevel } from "@src/utilities/permission/types";
+
+export class LevelCommand implements Command {
+  name = 'level'
+  permissions = 0
+  description = 'Displays your current level'
+  requiredParams = []
+  optionalParams = []
+  alias = ['lvl', 'permission', 'permissions']
+  cooldown = 5000
+  execute = async (
+    channel: string,
+    userstate: TwitchUserState,
+    message: string[]
+  ): Promise<BotResponse> => {
+    return {
+      success: true,
+      channel,
+      response: ''
+    }
+  }
+  methods = {
+    getDatabasePermissions: async (id: number): Promise<number> => {
+      const permissions = (
+        await hb.db.userRepo.findOneBy({
+          id: id
+        })
+      )?.permission
+
+      if (!permissions) return PermissionLevel.USER
+
+      return permissions
+    },
+    getUserPermissions: ({ permission }: TwitchUserState): number => {
+      if (!permission) return PermissionLevel.USER
+
+      return permission
+    },
+
+    mapToPermissionName: (permissionslevel: PermissionLevel): string => {
+      return PermissionLevel[permissionslevel].toLowerCase()
+    }
+  }
+}
