323fbd382d565088bd9f1d612a4e72c6e4f876c0
helltf <Dario.Zachow5530@gmail.com>
2022-06-12 18:35:35 +0200

    chore: util function to genate all combinations of arrays

--- src/utilities/utility.ts
+++ src/utilities/utility.ts
@@ -10,4 +10,8 @@ export class Utility {
   random = (lowerLimit = 0, upperLimit = 1): number => {
     return Math.floor(Math.random() * (upperLimit + lowerLimit + 1))
   }
+
+  generateAllCombinations(arr1: any[], arr2: any[]): any[][] {
+    return arr1.flatMap(d => arr2.map(v => [d, v]))
+  }
 }
