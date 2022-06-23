afe7c7274b994f6555adf97b77080a15049bdbdd
helltf <dario.zachow5530@gmail.com>
2022-06-23 13:05:47 +0200

    feat: adding command and unit tests for methods

--- src/commands/cmd/uid.ts
+++ src/commands/cmd/uid.ts
@@ -1,5 +1,5 @@
-import { TwitchUserState, BotResponse } from "../../client/types.js";
-import { Command } from "../types.js";
+import { TwitchUserState, BotResponse } from "@src/client/types";
+import { Command } from "@src/commands/types";
 
 export class UidCommand implements Command {
     name: string = 'uid';
@@ -9,8 +9,36 @@ export class UidCommand implements Command {
     optionalParams: string[] = ['user'];
     alias: string[] = ['userid'];
     cooldown: number = 10000;
-    execute = (channel: string, userstate: TwitchUserState, message: string[]): Promise<BotResponse> => {
+    execute = async (channel: string, userstate: TwitchUserState, [searchUser]: string[]): Promise<BotResponse> => {
 
+        if (!searchUser)
+            return {
+                channel,
+                response: this.methods.getResponse(userstate["user-id"], userstate.username),
+                success: true
+            }
+
+        const id = await this.methods.getId(searchUser)
+
+        if (!id)
+            return {
+                channel,
+                response: 'Could not find user',
+                success: false
+            }
+
+        return {
+            channel,
+            response: this.methods.getResponse(id, searchUser),
+            success: true
+        }
     }
-
+    methods = {
+        getResponse(id?: number | string, username?: string): string {
+            return `${username}👉${id}`
+        },
+        getId(username: string): Promise<number | undefined> {
+            return hb.api.twitch.getUserIdByName(username)
+        }
+    }
 }
--- test/unit/commands/uid.spec.ts
+++ test/unit/commands/uid.spec.ts
@@ -1,14 +1,63 @@
-import { UidCommand } from "../../../src/commands/cmd/uid.js"
-import { setup } from "../../test-utils/setup.js"
+import { UidCommand } from "@src/commands/cmd/uid"
+import { getExampleTwitchUserState } from "@test-utils/example"
+import { setup } from "@test-utils/setup"
 
-describe('test uid command', () => {
+fdescribe('test uid command', () => {
     const uid = new UidCommand()
+    const channel = 'channel'
+    const user = getExampleTwitchUserState({})
 
     beforeAll(() => {
         setup()
     })
 
-    it('no user is given return usernames id', () => {
+    it('no user is given return usernames id', async () => {
+        const message: string[] = []
+        const id = user["user-id"]!
 
+        const {
+            channel: responseChannel,
+            response,
+            success
+        } = await uid.execute(channel, user, message)
+
+        expect(responseChannel).toBe(channel)
+        expect(response).toBe(`${user.username}👉${id}`)
+        expect(success).toBeTrue()
+
+    })
+
+    it('user is given return user id from api', async () => {
+        const searchedUser = 'searchedUser'
+        const message = [searchedUser]
+        const id = 300
+        spyOn(hb.api.twitch, 'getUserIdByName').and.resolveTo(id)
+
+        const {
+            channel: responseChannel,
+            response,
+            success
+        } = await uid.execute(channel, user, message)
+
+        expect(responseChannel).toBe(channel)
+        expect(response).toBe(`${searchedUser}👉${id}`)
+        expect(success).toBeTrue()
+    })
+
+    it('user can\'t be found return error response', async () => {
+        const searchedUser = 'searchedUser'
+        const message = [searchedUser]
+        const id = undefined
+        spyOn(hb.api.twitch, 'getUserIdByName').and.resolveTo(id)
+
+        const {
+            channel: responseChannel,
+            response,
+            success
+        } = await uid.execute(channel, user, message)
+
+        expect(responseChannel).toBe(channel)
+        expect(response).toBe(`Could not find user`)
+        expect(success).toBeFalse()
     })
 })
