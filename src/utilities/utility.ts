0f49ef71e9a3122336505db197b8e1c3c45c6103
helltf <dario.zachow5530@gmail.com>
2022-06-26 14:27:54 +0200

    chore: format time properly

--- src/utilities/utility.ts
+++ src/utilities/utility.ts
@@ -1,5 +1,6 @@
 import { Permission } from '@src/utilities/permission/permission'
-
+import { time, timeStamp } from 'console'
+import { HumanizeDuration, HumanizeDurationLanguage } from 'humanize-duration-ts'
 export class Utility {
   permission: Permission
 
@@ -19,5 +20,23 @@ export class Utility {
     return Object.keys(anyEnum)
       .filter(v => !isNaN(Number(v)))
       .map(v => Number(v))
+  }
+
+  humanize(time: number): string {
+    const formatter = new HumanizeDuration(new HumanizeDurationLanguage())
+    return this.shortenTimeString(formatter.humanize(time, { round: true }))
+  }
+
+  shortenTimeString(timeString: string) {
+    return timeString
+      .replace(/years?/, 'y')
+      .replace(/months?/, 'm')
+      .replace(/weeks?/, 'w')
+      .replace(/days?/, 'd')
+      .replace(/hours?/, 'h')
+      .replace(/minutes?/, 'min')
+      .replace(/seconds?/, 's')
+      .replace(/\s/g, '')
+      .replace(/,/g, ' ')
   }
 }
