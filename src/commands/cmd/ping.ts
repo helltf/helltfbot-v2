47304bd30a67d9345b295443ddad9da8a51a6525
helltf <dario.zachow5530@gmail.com>
2022-06-26 15:56:25 +0200

    feat: retrieving latency, memory usage, channels, commands as data

--- src/commands/cmd/ping.ts
+++ src/commands/cmd/ping.ts
@@ -1,6 +1,5 @@
 import { Command } from '../types'
 import { BotResponse } from '../../client/types'
-import * as os from 'os'
 export class PingCommand implements Command {
   name = 'ping'
   description = 'just a ping command'
@@ -11,16 +10,43 @@ export class PingCommand implements Command {
   alias = []
   async execute(): Promise<BotResponse> {
     const uptime = this.methods.getUptime()
-    console.log(hb.utils.humanize(Math.floor(os.uptime() * 1000)))
+    const memoryUsage = this.methods.getMemory()
+    const commandsIssued = await this.methods.getCommandsIssued()
+    const joinedChannels = await this.methods.getChannels()
+    const latency = await this.methods.getLatency()
+
     return {
-      response: `pong | Uptime: ${uptime}`,
+      response: `pong | Latency: ${latency}ms | Uptime: ${uptime} | Memory used: ${memoryUsage} | Commands issued: ${commandsIssued} | Connected to ${joinedChannels} channels`,
       success: true
     }
   }
+
   methods = {
     getUptime: (): string => {
       const uptime = Math.floor(process.uptime()) * 1000
       return hb.utils.humanize(uptime)
+    },
+
+    getMemory: (): string => this.methods.formatMemoryUsage(process.memoryUsage().heapUsed),
+    formatMemoryUsage: (data: number) => `${Math.floor(Math.round(data / 1024 / 1024 * 100) / 100)} MB`,
+
+    getCommandsIssued: async (): Promise<number> => {
+      return Number(
+        (await hb.db.commandRepo
+          .createQueryBuilder('commands')
+          .select('SUM(commands.counter)', 'sum')
+          .getRawOne())
+          ?.sum)
+    },
+
+    getChannels: async (): Promise<number> => {
+      return hb.db.channelRepo.countBy({
+        joined: true
+      })
+    },
+
+    getLatency: async (): Promise<number> => {
+      return (await hb.client.ping())[0] * 1000
     }
   }
 }
