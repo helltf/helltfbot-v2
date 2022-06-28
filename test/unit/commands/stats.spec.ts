088ea3b96cf547b4b874a2e17e0673d4ac261e2f
helltf <dario.zachow5530@gmail.com>
2022-06-28 16:43:37 +0200

    test: validate type input

---
+++ test/unit/commands/stats.spec.ts
@@ -0,0 +1,28 @@
+import { StatsCommand, StatsType } from "@src/commands/cmd/stats"
+
+describe('stats command', () => {
+    let stats: StatsCommand
+    const validTypes = Object.values(StatsType)
+
+    beforeEach(() => {
+        stats = new StatsCommand()
+    })
+
+    describe('isvalid function', () => {
+        validTypes.forEach(type => {
+            it(`${type} is valid return true`, () => {
+                const result = stats.methods.isValidType(type)
+
+                expect(result).toBeTrue()
+            })
+        })
+
+        it('type is incorrect return false', () => {
+            const type = 'sfds'
+            const result = stats.methods.isValidType(type)
+
+            expect(result).toBeFalse()
+        })
+    })
+
+})
