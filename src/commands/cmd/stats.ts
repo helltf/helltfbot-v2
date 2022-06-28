a53ce49d14d793fab076cbafaa0d38baf8eb18fc
helltf <dario.zachow5530@gmail.com>
2022-06-28 16:44:03 +0200

    feat: retrieving stats for user and given param

--- src/commands/cmd/stats.ts
+++ src/commands/cmd/stats.ts
@@ -10,11 +10,58 @@ export class StatsCommand implements Command {
     optionalParams: string[] = ['user'];
     alias: string[] = ['statistics'];
     cooldown: number = 20000;
-    execute = async (context: Context): Promise<BotResponse> => {
+    execute = async ({ message: [type, lookup], user }: Context): Promise<BotResponse> => {
+        const username = lookup !== undefined ? lookup : user.username!
+
+        if (!this.methods.isValidType(type))
+            return {
+                response: `Valid stats are ${Object.values(StatsType)}`,
+                success: false
+            }
+
+        if (type === StatsType.EMOTEGAME)
+            return await this.methods.getEmotegameStats(username)
+
         return {
-            response: '',
-            success: true
+            response: 'unknown error',
+            success: false
         }
     }
 
+    methods = {
+        isValidType(type: string): boolean {
+            return hb.utils.enumContains(StatsType, type)
+        },
+
+        getEmotegameStats: async (username: string): Promise<BotResponse> => {
+            const stats = await hb.db.emoteStatsRepo.findOneBy({
+                user: {
+                    name: username
+                }
+            })
+
+            if (!stats)
+                return {
+                    success: false,
+                    response: `${username} has no stats recorded`
+                }
+
+            const { emotes_guessed,
+                incorrect_guesses,
+                letters_guessed } = stats
+
+            return {
+                response: [
+                    `${username} guessed ${letters_guessed} letters`,
+                    `${emotes_guessed} emotes`,
+                    `${incorrect_guesses} times incorrect`
+                ],
+                success: true
+            }
+        }
+    }
+}
+
+export enum StatsType {
+    EMOTEGAME = 'emotegame      '
 }
