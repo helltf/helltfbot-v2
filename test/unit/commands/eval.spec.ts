09592586358135ad10aa3f14395103a4e309abf9
helltf <Dario.Zachow5530@gmail.com>
2022-06-30 12:29:37 +0200

    chore: removing return from examples

--- test/unit/commands/eval.spec.ts
+++ test/unit/commands/eval.spec.ts
@@ -2,55 +2,55 @@ import { EvalCommand } from "@src/commands/cmd/eval"
 import { getExampleTwitchUserState } from "@test-utils/example"
 
 describe('eval command', () => {
-    let evalcommand: EvalCommand
-    const user = getExampleTwitchUserState({})
-    beforeEach(() => {
-        evalcommand = new EvalCommand()
+  let evalcommand: EvalCommand
+  const user = getExampleTwitchUserState({})
+  beforeEach(() => {
+    evalcommand = new EvalCommand()
+  })
+
+  it('code throws error return error', async () => {
+    const error = 'error'
+    const code = `throw new Error('${error}')`
+
+    const { response, success } = await evalcommand.execute({
+      channel: 'channel',
+      message: [code],
+      user: user
     })
 
-    it('code throws error return error', async () => {
-        const error = 'error'
-        const code = `throw new Error('${error}')`
+    expect(success).toBeFalse()
+    expect(response).toBeDefined()
+  })
 
-        const { response, success } = await evalcommand.execute({
-            channel: 'channel',
-            message: [code],
-            user: user
-        })
+  it('code throws no error return success', async () => {
+    const returnValue = 'abc'
+    const code = `'${returnValue}'`
 
-        expect(success).toBeFalse()
-        expect(response).toBeDefined()
+    const { response, success } = await evalcommand.execute({
+      channel: 'channel',
+      message: [code],
+      user: user
     })
 
-    it('code throws no error return success', async () => {
-        const returnValue = 'abc'
-        const code = `return '${returnValue}'`
+    expect(success).toBeTrue()
+    expect(response).toBe(returnValue)
+  })
 
-        const { response, success } = await evalcommand.execute({
-            channel: 'channel',
-            message: [code],
-            user: user
-        })
-
-        expect(success).toBeTrue()
-        expect(response).toBe(returnValue)
+  it('code returns object stringify object', async () => {
+    const returnValue = JSON.stringify({
+      a: 1,
+      b: 'asd'
     })
 
-    it('code returns object stringify object', async () => {
-        const returnValue = JSON.stringify({
-            a: 1,
-            b: "asd"
-        })
+    const code = `'${returnValue}'`
 
-        const code = `return '${returnValue}'`
-
-        const { response, success } = await evalcommand.execute({
-            channel: 'channel',
-            message: [code],
-            user: user
-        })
-
-        expect(success).toBeTrue()
-        expect(response).toBe(returnValue)
+    const { response, success } = await evalcommand.execute({
+      channel: 'channel',
+      message: [code],
+      user: user
     })
+
+    expect(success).toBeTrue()
+    expect(response).toBe(returnValue)
+  })
 })
