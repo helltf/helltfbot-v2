cf3b58a2f5aebbb95f348f7e779b0e60d1349298
helltf <Dario.Zachow5530@gmail.com>
2022-07-23 22:32:42 +0200

    feat: correcting algorithm for encrypting and decrypting

--- src/utilities/crypto.ts
+++ src/utilities/crypto.ts
@@ -1,21 +1,21 @@
 import { BinaryLike, CipherKey, createCipheriv, createDecipheriv, randomBytes } from "crypto"
 
+const algorithm = 'aes-256-cbc'
+
 export class CryptoUtility {
-    key: CipherKey
-    iv: BinaryLike
-    constructor(key: string = process.env.ENCRYPT_KEY) {
-        this.key = randomBytes(32);
-        this.iv = Buffer.alloc(8, 0);
-    }
-    encrypt(message: string, iv: BinaryLike): string {
-        const cipher = createCipheriv('aes-256-gcm', this.key, this.iv)
+  key: CipherKey
+  iv: BinaryLike
+  constructor(key: string = process.env.ENCRYPT_KEY) {
+    this.key = key
+  }
+  encrypt(message: string, iv: BinaryLike): string {
+    const cipher = createCipheriv(algorithm, this.key, iv)
+    return cipher.update(message, 'utf-8', 'hex') + cipher.final('hex')
+  }
 
-        return cipher.update(message, 'utf-8', 'hex') + cipher.final('hex')
-    }
+  decrypt(message: string, iv: BinaryLike): string {
+    const decipher = createDecipheriv(algorithm, this.key, iv)
 
-    decrypt(message: string, iv: BinaryLike): string {
-        const decipher = createDecipheriv('aes-256-gcm', this.key, this.iv)
-
-        return decipher.update(message, 'utf-8', 'hex') + decipher.final('hex')
-    }
+    return decipher.update(message, 'hex', 'utf-8') + decipher.final('utf8')
+  }
 }
--- test/unit/utilities/crypto.spec.ts
+++ test/unit/utilities/crypto.spec.ts
@@ -2,20 +2,19 @@ import { CryptoUtility } from "@src/utilities/crypto"
 import { randomBytes } from "crypto"
 
 fdescribe('cryptoUtil utilities', () => {
-    let cryptoUtil: CryptoUtility
+  let cryptoUtil: CryptoUtility
 
-    beforeEach(() => {
-        cryptoUtil = new CryptoUtility()
-    })
+  beforeEach(() => {
+    cryptoUtil = new CryptoUtility()
+  })
 
-    it('cryptoUtil encrypts file with key and decrypts to same result', () => {
-        const exampleMessage = 'Hello World'
-        const iv = 'sadasd'
+  it('cryptoUtil encrypts file with key and decrypts to same result', () => {
+    const exampleMessage = 'Hello World'
+    const iv = randomBytes(16)
 
-        const encryptedMessage = cryptoUtil.encrypt(exampleMessage, iv)
-        console.log(encryptedMessage)
-        const resultMessage = cryptoUtil.decrypt(encryptedMessage, iv)
+    const encryptedMessage = cryptoUtil.encrypt(exampleMessage, iv)
+    const resultMessage = cryptoUtil.decrypt(encryptedMessage, iv)
 
-        expect(exampleMessage).toBe(resultMessage)
-    })
+    expect(exampleMessage).toBe(resultMessage)
+  })
 })
