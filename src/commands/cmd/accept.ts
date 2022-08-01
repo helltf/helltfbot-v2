1270ff1a6485eafd4dfdd86064c58b126cf468e2
helltf <Dario.Zachow5530@gmail.com>
2022-08-01 20:40:59 +0200

    feat: updating suggestion

--- src/commands/cmd/accept.ts
+++ src/commands/cmd/accept.ts
@@ -1,5 +1,6 @@
 import { BotResponse } from "@src/client/types";
 import { Command, CommandContext, CommandFlag } from "@src/commands/types";
+import { SuggestionStatus } from "@src/db/entity/suggestion.entity";
 import {
   ChatPermissionLevel,
   GlobalPermissionLevel
@@ -9,15 +10,37 @@ export class AcceptCommand implements Command {
   name = 'accept'
   permissions = GlobalPermissionLevel.ADMIN
   description = 'Accepts the given suggestion with a reason'
-  requiredParams = ['id', 'reason']
-  optionalParams = []
+  requiredParams = ['id']
+  optionalParams = ['reason']
   alias = ['acceptsuggestion']
   flags = [CommandFlag.WHISPER]
   cooldown = 0
-  execute = async (context: CommandContext): Promise<BotResponse> => {
+  execute = async ({
+    message: [id, ...reason]
+  }: CommandContext): Promise<BotResponse> => {
+    if (!id)
+      return {
+        response: 'id missing',
+        success: false
+      }
+
+    const success = await this.methods.updateSuggestion(id, reason.join(' '))
+
     return {
-      response: 'id missing',
-      success: false
+      success: success,
+      response: success ? 'Updated suggestion' : 'suggestion does not exist'
+    }
+  }
+  methods = {
+    updateSuggestion: async (id: string, reason: string): Promise<boolean> => {
+      const result = await hb.db.suggestion.update(
+        {
+          id: Number(id)
+        },
+        { status: SuggestionStatus.ACCEPTED, reason: reason }
+      )
+
+      return result.affected !== 0
     }
   }
 }
