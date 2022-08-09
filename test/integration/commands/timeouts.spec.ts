093e9d55307acd284bdbaf503e3002f6fd2c7ad8
helltf <Dario.Zachow5530@gmail.com>
2022-08-09 09:08:31 +0200

    chore: removing fdescribe

--- test/integration/commands/timeouts.spec.ts
+++ test/integration/commands/timeouts.spec.ts
@@ -1,128 +1,130 @@
 import { BanEntity, TwitchUserEntity } from "@db/entities"
 import { TwitchUserState } from "@src/client/types"
 import { TimeoutsCommand } from "@src/commands/cmd/timeouts"
-import { clearDb } from "@test-utils/clear"
-import { disconnectDatabase } from "@test-utils/disconnect"
-import { getExampleTwitchUserEntity, getExampleTwitchUserState } from "@test-utils/example"
-import { setupDatabase } from "@test-utils/setup-db"
-import { time } from "console"
-import { hkdf } from "crypto"
+import { clearDb } from '@test-utils/clear'
+import { disconnectDatabase } from '@test-utils/disconnect'
+import {
+  getExampleTwitchUserEntity,
+  getExampleTwitchUserState
+} from '@test-utils/example'
+import { setupDatabase } from '@test-utils/setup-db'
+import { time } from 'console'
+import { hkdf } from 'crypto'
 
 describe('test suggest command', () => {
-    let messageUser: TwitchUserState
-    let timeouts: TimeoutsCommand
-    const channel = 'channel'
-    beforeAll(async () => {
-        await setupDatabase()
+  let messageUser: TwitchUserState
+  let timeouts: TimeoutsCommand
+  const channel = 'channel'
+  beforeAll(async () => {
+    await setupDatabase()
+  })
+
+  beforeEach(async () => {
+    messageUser = getExampleTwitchUserState({})
+    timeouts = new TimeoutsCommand()
+    await clearDb(hb.db.dataSource)
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
+  })
+
+  afterAll(async () => {
+    await disconnectDatabase()
+  })
+
+  describe('execute', () => {
+    it('no param given return no bans because user has no bans', async () => {
+      const { response, success } = await timeouts.execute({
+        channel: channel,
+        message: [],
+        user: messageUser
+      })
+
+      expect(success).toBeTrue()
+      expect(response).toBe('No timeout found')
     })
 
-    beforeEach(async () => {
-        messageUser = getExampleTwitchUserState({})
-        timeouts = new TimeoutsCommand()
-        await clearDb(hb.db.dataSource)
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
+    it('no params message user has ban in channel', async () => {
+      const currentTime = Date.now()
+      const times = 1
+
+      await hb.db.ban.save({
+        at: currentTime,
+        channel: channel,
+        user: messageUser.username
+      })
+
+      const { response, success } = await timeouts.execute({
+        channel: channel,
+        message: [],
+        user: messageUser
+      })
+      const expectedResponse = [
+        `${messageUser.username} has been timeouted ${times} times in channel ${channel}`,
+        `Last ban: ${hb.utils.humanizeNow(currentTime)} ago`
+      ]
+      expect(success).toBeTrue()
+      expect(response).toEqual(expectedResponse)
     })
 
-    afterAll(async () => {
-        await disconnectDatabase()
+    it('no params message user has two bans in channel', async () => {
+      const currentTime = Date.now()
+      const times = 2
+
+      await hb.db.ban.save({
+        at: currentTime,
+        channel: channel,
+        user: messageUser.username
+      })
+
+      await hb.db.ban.save({
+        at: currentTime - 5000,
+        channel: channel,
+        user: messageUser.username
+      })
+
+      const { response, success } = await timeouts.execute({
+        channel: channel,
+        message: [],
+        user: messageUser
+      })
+
+      const expectedResponse = [
+        `${messageUser.username} has been timeouted ${times} times in channel ${channel}`,
+        `Last ban: ${hb.utils.humanizeNow(currentTime)} ago`
+      ]
+
+      expect(success).toBeTrue()
+      expect(response).toEqual(expectedResponse)
     })
 
-    fdescribe('execute', () => {
-        it('no param given return no bans because user has no bans', async () => {
-            const { response, success } = await timeouts.execute({
+    it('user is given return overall bans for user', async () => {
+      const username = 'givenUser'
+      const timeoutChannel = 'timeoutChannel'
+      const channels = 1
+      const amount = 1
+      const currentTime = Date.now()
+      const time_ago = hb.utils.humanizeNow(currentTime)
 
-                channel: channel,
-                message: [],
-                user: messageUser
-            })
+      await hb.db.ban.save({
+        at: currentTime,
+        channel: timeoutChannel,
+        user: username
+      })
 
-            expect(success).toBeTrue()
-            expect(response).toBe('No timeout found')
-        })
+      const { response, success } = await timeouts.execute({
+        user: messageUser,
+        channel: channel,
+        message: [username]
+      })
 
-        it('no params message user has ban in channel', async () => {
-            const currentTime = Date.now()
-            const times = 1
+      const expectedResponse = [
+        `${username} has been timeouted ${amount}`,
+        `${channels} different channels`,
+        `last timeout ${time_ago} ago in ${timeoutChannel}`
+      ]
 
-            await hb.db.ban.save({
-                at: currentTime,
-                channel: channel,
-                user: messageUser.username
-            })
-
-            const { response, success } = await timeouts.execute({
-
-                channel: channel,
-                message: [],
-                user: messageUser
-            })
-            const expectedResponse = [
-                `${messageUser.username} has been timeouted ${times} times in channel ${channel}`,
-                `Last ban: ${hb.utils.humanizeNow(currentTime)} ago`]
-            expect(success).toBeTrue()
-            expect(response).toEqual(expectedResponse)
-        })
-
-
-        it('no params message user has two bans in channel', async () => {
-            const currentTime = Date.now()
-            const times = 2
-
-            await hb.db.ban.save({
-                at: currentTime,
-                channel: channel,
-                user: messageUser.username
-            })
-
-            await hb.db.ban.save({
-                at: currentTime - 5000,
-                channel: channel,
-                user: messageUser.username
-            })
-
-            const { response, success } = await timeouts.execute({
-                channel: channel,
-                message: [],
-                user: messageUser
-            })
-
-            const expectedResponse = [
-                `${messageUser.username} has been timeouted ${times} times in channel ${channel}`,
-                `Last ban: ${hb.utils.humanizeNow(currentTime)} ago`]
-
-            expect(success).toBeTrue()
-            expect(response).toEqual(expectedResponse)
-        })
-
-        it('user is given return overall bans for user', async () => {
-            const username = 'givenUser'
-            const timeoutChannel = 'timeoutChannel'
-            const channels = 1
-            const amount = 1
-            const currentTime = Date.now()
-            const time_ago = hb.utils.humanizeNow(currentTime)
-
-            await hb.db.ban.save({
-                at: currentTime,
-                channel: timeoutChannel,
-                user: username
-            })
-
-            const { response, success } = await timeouts.execute({
-                user: messageUser,
-                channel: channel,
-                message: [username]
-            })
-
-            const expectedResponse = [
-                `${username} has been timeouted ${amount}`,
-                `${channels} different channels`,
-                `last timeout ${time_ago} ago in ${timeoutChannel}`
-            ]
-
-            expect(response).toEqual(expectedResponse)
-            expect(success).toBeTrue()
-        })
+      expect(response).toEqual(expectedResponse)
+      expect(success).toBeTrue()
     })
+  })
 })
 
