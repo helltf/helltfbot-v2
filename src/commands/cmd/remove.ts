f6d64df510d698effade2f8e84a5ca0d674e49ad
helltf <Dario.Zachow5530@gmail.com>
2022-08-10 16:18:22 +0200

    feat: command for removing 7tv emotes

--- src/commands/cmd/remove.ts
+++ src/commands/cmd/remove.ts
@@ -1,1 +1,28 @@
-export class RemoveCommand implements Command {}
+import { BotResponse } from "@src/client/types";
+import { Command, CommandContext, CommandFlag } from "@src/commands/types";
+import {
+  ChatPermissionLevel,
+  GlobalPermissionLevel
+} from '@src/utilities/permission/types'
+
+export class RemoveCommand implements Command {
+  name = 'remove'
+  permissions = ChatPermissionLevel.MOD
+  description = 'removes a 7tv emote from channel'
+  requiredParams = ['emote_name']
+  optionalParams = []
+  alias = ['removeemote']
+  flags = []
+  cooldown: 10000
+  execute = async ({
+    message: [emote],
+    channel
+  }: CommandContext): Promise<BotResponse> => {
+    const { success, error } = await hb.api.seventv.removeEmote(emote, channel)
+
+    return {
+      response: success ? `Succesfully removed ${emote}` : `${error}`,
+      success: success
+    }
+  }
+}
