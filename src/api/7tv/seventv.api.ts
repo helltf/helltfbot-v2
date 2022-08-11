e393ae4d6261383a99ed8a420d42a8c54898aa2b
helltf <Dario.Zachow5530@gmail.com>
2022-08-11 09:39:34 +0200

    feat(7tv-api): adding function to check if user is editor

--- src/api/7tv/seventv.api.ts
+++ src/api/7tv/seventv.api.ts
@@ -1,4 +1,4 @@
-import { Resource } from "../types"
+import { Resource, ResourceError, ResourceSuccess } from "../types"
 import { SevenTvGQL } from '@api/7tv/seventv.gql'
 import { SevenTvRest } from '@api/7tv/seventv.rest'
 
@@ -8,6 +8,23 @@ export class SevenTvApi {
 
   async getEmotesForChannel(channel: string): Promise<Resource<string[]>> {
     return this.rest.getEmotesForChannel(channel)
+  }
+
+  async isEditor(
+    username: string,
+    channel: string
+  ): Promise<Resource<boolean>> {
+    if (username === channel) {
+      return new ResourceSuccess(true)
+    }
+    const editors = await this.gql.getUserEditors(username)
+
+    if (editors instanceof ResourceError) {
+      return editors
+    }
+    return new ResourceSuccess(
+      editors.data.some(editor => editor.login === username.toLowerCase())
+    )
   }
 }
 
