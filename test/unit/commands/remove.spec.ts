4955004b1576aae3d17838182875ca057c252762
helltf <dario.zachow5530@gmail.com>
2022-08-14 20:12:57 +0200

    test(remove): testing add emote by id function

--- test/unit/commands/remove.spec.ts
+++ test/unit/commands/remove.spec.ts
@@ -1,3 +1,4 @@
+import { EmoteData } from "@api/7tv/seventv.gql"
 import { ResourceError, ResourceSuccess } from "@api/types"
 import { BotResponse } from "@src/client/types"
 import { RemoveCommand } from "@src/commands/cmd/remove"
@@ -5,6 +6,8 @@ import { getExampleTwitchUserState } from "@test-utils/example"
 
 fdescribe('remove command', () => {
     let remove: RemoveCommand
+    const emote = 'emote'
+    const channel = 'channel'
 
     beforeEach(() => {
         remove = new RemoveCommand()
@@ -26,7 +29,6 @@ fdescribe('remove command', () => {
         })
 
         it('editor request returns error return error response', async () => {
-            const emote = 'emote'
             const error = 'Error'
 
             spyOn(hb.api.seventv, 'isEditor').and.resolveTo(new ResourceError(error))
@@ -42,7 +44,6 @@ fdescribe('remove command', () => {
         })
 
         it('user is not an editor return error', async () => {
-            const emote = 'emote'
             spyOn(hb.api.seventv, 'isEditor').and.resolveTo(new ResourceSuccess(false))
 
             const { response, success } = await remove.execute({
@@ -56,7 +57,6 @@ fdescribe('remove command', () => {
         })
 
         it('user is editor invoke addEmoteByName with given args and return response', async () => {
-            const emote = 'emote'
             const botResponse: BotResponse = {
                 response: 'response',
                 success: false
@@ -76,7 +76,7 @@ fdescribe('remove command', () => {
 
         it('input is url invoke addEmoteById with id and return response', async () => {
             const id = '60e0ec549db74f240c4c0c5B'
-            const emote = `https://7tv.app/emotes/${id}`
+            const emoteUrl = `https://7tv.app/emotes/${id}`
             const botResponse: BotResponse = {
                 response: 'response',
                 success: false
@@ -85,7 +85,7 @@ fdescribe('remove command', () => {
             spyOn(remove.methods, 'addEmoteById').and.resolveTo(botResponse)
 
             const response = await remove.execute({
-                message: [emote],
+                message: [emoteUrl],
                 channel,
                 user
             })
@@ -96,9 +96,6 @@ fdescribe('remove command', () => {
     })
 
     describe('add emote by name', () => {
-        const emote = 'emote'
-        const channel = 'channel'
-
         it('remove emote request returns error return error response', async () => {
             const error = 'Error'
             spyOn(hb.api.seventv.gql, 'removeEmote').and.resolveTo(new ResourceError(error))
@@ -106,6 +103,58 @@ fdescribe('remove command', () => {
             const { response, success } = await remove.methods.addEmoteByName(emote, channel)
 
             expect(response).toBe(error)
+            expect(success).toBeFalse()
+        })
+
+        it('remove emote request is successful return success response', async () => {
+            const emoteData = { id: '1', name: emote }
+
+            spyOn(hb.api.seventv.gql, 'removeEmote').and.resolveTo(new ResourceSuccess(emoteData))
+
+            const { response, success } = await remove.methods.addEmoteByName(emote, channel)
+
+            expect(response).toBe(`Successfully removed ${emoteData.name}`)
+            expect(success).toBeTrue()
+        })
+    })
+
+    describe('add emote by id', () => {
+        const emoteId = '1'
+
+        it('channel does not exist return error response', async () => {
+            const error = 'Error'
+
+            spyOn(hb.api.seventv.rest, 'getUserId').and.resolveTo(new ResourceError(error))
+
+            const { response, success } = await remove.methods.addEmoteById(emoteId, channel)
+
+            expect(response).toBe(error)
+            expect(success).toBeFalse()
+        })
+
+        it('adding emotes failes return error response', async () => {
+            const error = 'Error'
+
+            spyOn(hb.api.seventv.rest, 'getUserId').and.resolveTo(new ResourceSuccess('2'))
+            spyOn(hb.api.seventv.gql, 'removeEmoteById').and.resolveTo(new ResourceError(error))
+
+            const { response, success } = await remove.methods.addEmoteById(emoteId, channel)
+
+            expect(response).toBe(error)
+            expect(success).toBeFalse()
+        })
+
+        it('adding emote is successful return success response', async () => {
+            const emoteData: EmoteData = {
+                id: emoteId,
+                name: emote
+            }
+            spyOn(hb.api.seventv.rest, 'getUserId').and.resolveTo(new ResourceSuccess('2'))
+            spyOn(hb.api.seventv.gql, 'removeEmoteById').and.resolveTo(new ResourceSuccess(emoteData))
+
+            const { response, success } = await remove.methods.addEmoteById(emoteId, channel)
+
+            expect(response).toBe(`Successfully removed ${emoteData.name}`)
             expect(success).toBeFalse()
         })
     })
