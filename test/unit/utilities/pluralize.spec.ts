3a1d7200635e14b195c25c45c800e0d7e99513a5
helltf <dario.zachow5530@gmail.com>
2022-08-15 18:40:43 +0200

    test(utils): adding test for pluralize functions

---
+++ test/unit/utilities/pluralize.spec.ts
@@ -0,0 +1,31 @@
+import { setup } from "@test-utils/setup"
+
+describe('pluralize', () => {
+    beforeEach(setup)
+
+    it('pluralize appends s to input', () => {
+        const input = 'test'
+        const result = hb.utils.plularize(input)
+
+        expect(result).toBe(input + 's')
+    })
+})
+
+describe('pluralize if', () => {
+    it('value is not 1 return input in plural', () => {
+        const input = 'test'
+        const someNumber = 1
+        const result = hb.utils.plularizeIf(input, someNumber)
+
+        expect(result).toBe(input + 's')
+    })
+
+    it('value is 1 return same input', () => {
+        const input = 'Test'
+        const someNumber = 0
+
+        const result = hb.utils.plularizeIf(input, someNumber)
+
+        expect(result).toBe(input)
+    })
+})
