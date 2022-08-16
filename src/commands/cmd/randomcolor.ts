6faa6b815aab60246bd12c960dca2d4713fe8352
helltf <Dario.Zachow5530@gmail.com>
2022-08-16 21:16:20 +0200

    feat(randomcolor): generating random colors per command

---
+++ src/commands/cmd/randomcolor.ts
@@ -0,0 +1,47 @@
+import { BotResponse } from '@src/client/types';
+import { Command, CommandContext, CommandFlag } from '@src/commands/types'
+import {
+  ChatPermissionLevel,
+  GlobalPermissionLevel
+} from '@src/utilities/permission/types'
+
+export class RandomColorCommand implements Command {
+  name = 'randomcolor'
+  permissions = ChatPermissionLevel.USER
+  description = 'generates a random hex color'
+  requiredParams = []
+  optionalParams = []
+  alias = ['rc']
+  flags = [CommandFlag.WHISPER]
+  cooldown = 10000
+  execute = async (context: CommandContext): Promise<BotResponse> => {}
+
+  methods = {
+    generateHex: (): string => {
+      const chars = Array(6).map(
+        () => this.static.chars[hb.utils.random(0, this.static.chars.length)]
+      )
+      return '#'
+    }
+  }
+
+  static = {
+    chars: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F'
+    ]
+  }
+}
