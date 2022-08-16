6da37836568a0264d3e354002fc8b13f5fb38af0
helltf <Dario.Zachow5530@gmail.com>
2022-08-16 21:15:21 +0200

    test(randomcolor): testing execute and generate function

---
+++ test/unit/commands/randomcolor.spec.ts
@@ -0,0 +1,36 @@
+import { RandomColorCommand } from "@src/commands/cmd/randomcolor"
+import { getExampleTwitchUserState } from "@test-utils/example"
+import { setup } from "@test-utils/setup"
+
+describe('random color', () => {
+  let randomColor: RandomColorCommand
+  const user = getExampleTwitchUserState({})
+  const channel = 'channel'
+
+  beforeEach(() => {
+    setup()
+    randomColor = new RandomColorCommand()
+  })
+
+  it('return a random generated hexcode', async () => {
+    const returnedCode = '#11111'
+
+    spyOn(randomColor.methods, 'generateHex').and.returnValue(returnedCode)
+
+    const { response, success } = await randomColor.execute({
+      channel,
+      user,
+      message: []
+    })
+
+    expect(response).toBe(`${returnedCode}`)
+    expect(success).toBeTrue()
+  })
+
+  it('generate function returns a string with 7 chars', () => {
+    const color = randomColor.methods.generateHex()
+
+    expect(color).toBeDefined()
+    expect(color.length).toBe(7)
+  })
+})
