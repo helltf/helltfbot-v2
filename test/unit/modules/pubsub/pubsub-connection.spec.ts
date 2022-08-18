b0f101fa7f2b7fe39411e256bbb144dde434f02d
helltf <Dario.Zachow5530@gmail.com>
2022-08-18 08:03:21 +0200

    chore(pubsub-tests): reenabling tests

--- test/unit/modules/pubsub/pubsub-connection.spec.ts
+++ test/unit/modules/pubsub/pubsub-connection.spec.ts
@@ -4,10 +4,11 @@ import { Topic, TopicPrefix } from '@modules/pubsub/types'
 import { TwitchBot } from 'bot'
 import * as WS from 'ws'
 
-describe.skip('test pubsub connection class', () => {
+describe('test pubsub connection class', () => {
   let connection: PubSubConnection
   let mockedWS: ReconnectingWebSocket
   jest.mock('reconnecting-websocket')
+  jest.useFakeTimers()
   beforeEach(() => {
     mockedWS = {
       addEventListener: jest.fn(),
@@ -18,127 +19,127 @@ describe.skip('test pubsub connection class', () => {
     connection = new PubSubConnection(mockedWS)
   })
 
-  // it('connection does not listen to topic return false', () => {
-  //   const topic = {
-  //     id: 1,
-  //     prefix: TopicPrefix.STATUS
-  //   }
-  //   const result = connection.containsTopic(topic)
+  it('connection does not listen to topic return false', () => {
+    const topic = {
+      id: 1,
+      prefix: TopicPrefix.STATUS
+    }
+    const result = connection.containsTopic(topic)
 
-  //   expect(result).toBe(false)
-  // })
+    expect(result).toBe(false)
+  })
 
-  // it('connection contains topic return true', () => {
-  //   const topic = {
-  //     id: 1,
-  //     prefix: TopicPrefix.STATUS
-  //   }
-  //   connection.topics.push(topic)
-  //   const result = connection.containsTopic(topic)
+  it('connection contains topic return true', () => {
+    const topic = {
+      id: 1,
+      prefix: TopicPrefix.STATUS
+    }
+    connection.topics.push(topic)
+    const result = connection.containsTopic(topic)
 
-  //   expect(result).toBe(true)
-  // })
+    expect(result).toBe(true)
+  })
 
   it('listener should be appended on creation', () => {
     expect(mockedWS.addEventListener).toHaveBeenCalled()
   })
 
-  // it('set ping interval sends a ping at least in a 5 minute period', () => {
-  //   jest.useFakeTimers()
+  it('set ping interval sends a ping at least in a 5 minute period', () => {
+    jest.useFakeTimers()
 
-  //   connection.setPingInterval()
+    connection.setPingInterval()
 
-  //   jest.advanceTimersByTime(1000 * 60 * 5)
+    jest.advanceTimersByTime(1000 * 60 * 5)
 
-  //   expect(connection.connection.send).toHaveBeenCalled()
-  // })
+    expect(connection.connection.send).toHaveBeenCalled()
+  })
 
-  // describe('remove topic', () => {
-  //   it('no topics existing do nothing', () => {
-  //     connection.topics = []
+  describe('remove topic', () => {
+    it('no topics existing do nothing', () => {
+      connection.topics = []
 
-  //     connection.removeTopics([])
+      connection.removeTopics([])
 
-  //     expect(connection.topics).toEqual([])
-  //   })
+      expect(connection.topics).toEqual([])
+    })
 
-  //   it('one topic existing but no removed stays the same', () => {
-  //     const topics: Topic[] = [
-  //       {
-  //         id: 1,
-  //         prefix: TopicPrefix.SETTING
-  //       }
-  //     ]
-  //     connection.topics = topics
+    it('one topic existing but no removed stays the same', () => {
+      const topics: Topic[] = [
+        {
+          id: 1,
+          prefix: TopicPrefix.SETTING
+        }
+      ]
+      connection.topics = topics
 
-  //     connection.removeTopics([])
+      connection.removeTopics([])
 
-  //     expect(connection.topics).toEqual(topics)
-  //   })
+      expect(connection.topics).toEqual(topics)
+    })
 
-  //   it('one topics existing and gets removed', () => {
-  //     const topic = {
-  //       id: 1,
-  //       prefix: TopicPrefix.SETTING
-  //     }
-  //     const topics = [topic]
+    it('one topics existing and gets removed', () => {
+      const topic = {
+        id: 1,
+        prefix: TopicPrefix.SETTING
+      }
+      const topics = [topic]
 
-  //     connection.topics = topics
+      connection.topics = topics
 
-  //     connection.removeTopics([topic])
+      connection.removeTopics([topic])
 
-  //     expect(connection.topics).toHaveLength(0)
-  //   })
+      expect(connection.topics).toHaveLength(0)
+    })
 
-  //   it('two topics existing and removing one', () => {
-  //     const topic1 = {
-  //       id: 1,
-  //       prefix: TopicPrefix.SETTING
-  //     }
-  //     const topic2 = {
-  //       id: 2,
-  //       prefix: TopicPrefix.SETTING
-  //     }
-  //     const topics = [topic1, topic2]
-  //     connection.topics = topics
+    it('two topics existing and removing one', () => {
+      const topic1 = {
+        id: 1,
+        prefix: TopicPrefix.SETTING
+      }
+      const topic2 = {
+        id: 2,
+        prefix: TopicPrefix.SETTING
+      }
+      const topics = [topic1, topic2]
+      connection.topics = topics
 
-  //     connection.removeTopics([topic1])
+      connection.removeTopics([topic1])
 
-  //     expect(connection.topics).toEqual([topic2])
-  //   })
+      expect(connection.topics).toEqual([topic2])
+    })
 
-  //   it('two topics for the same id only one gets removed', () => {
-  //     const topic1 = {
-  //       id: 1,
-  //       prefix: TopicPrefix.SETTING
-  //     }
-  //     const topic2 = {
-  //       id: 2,
-  //       prefix: TopicPrefix.STATUS
-  //     }
-  //     const topics = [topic1, topic2]
-  //     connection.topics = topics
+    it('two topics for the same id only one gets removed', () => {
+      const topic1 = {
+        id: 1,
+        prefix: TopicPrefix.SETTING
+      }
+      const topic2 = {
+        id: 2,
+        prefix: TopicPrefix.STATUS
+      }
+      const topics = [topic1, topic2]
+      connection.topics = topics
 
-  //     connection.removeTopics([topic1])
+      connection.removeTopics([topic1])
 
-  //     expect(connection.topics).toEqual([topic2])
-  //   })
-  // })
+      expect(connection.topics).toEqual([topic2])
+    })
+  })
 
-  // it('unlisten command should send unlisten to pubsub', () => {
-  //   globalThis.hb = new TwitchBot()
-  //   const topic = {
-  //     id: 1,
-  //     prefix: TopicPrefix.SETTING
-  //   }
-  //   const topics = [topic]
-  //   connection.topics = topics
-  //   jest.spyOn(connection, 'sendMessage')
-  //   jest.spyOn(connection, 'removeTopics')
+  it('unlisten command should send unlisten to pubsub', () => {
+    globalThis.hb = new TwitchBot()
+    const topic = {
+      id: 1,
+      prefix: TopicPrefix.SETTING
+    }
+    const topics = [topic]
+    connection.topics = topics
+    jest.spyOn(connection, 'sendMessage')
+    jest.spyOn(connection, 'removeTopics')
 
-  //   connection.unlisten(topics)
+    connection.unlisten(topics)
 
-  //   expect(connection.sendMessage).toHaveBeenCalled()
-  //   expect(connection.removeTopics).toHaveBeenCalledWith(topics)
-  // })
+    expect(connection.sendMessage).toHaveBeenCalled()
+    expect(connection.removeTopics).toHaveBeenCalledWith(topics)
+  })
 })
