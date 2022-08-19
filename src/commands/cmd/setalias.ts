4f8d674372989be98efb0ef3a39b86686868000c
helltf <dario.zachow5530@gmail.com>
2022-08-19 10:39:44 +0200

    refactor(setalias): checking for params and adding seperate method

--- src/commands/cmd/setalias.ts
+++ src/commands/cmd/setalias.ts
@@ -1,4 +1,4 @@
-import { ResourceError } from "@api/types";
+import { Resource, ResourceError, ResourceSuccess } from "@api/types";
 import { BotResponse } from "@src/client/types";
 import { Command, CommandContext } from "@src/commands/types";
 import { GlobalPermissionLevel } from '@src/utilities/permission/types'
@@ -17,38 +17,51 @@ export class SetAliasCommand implements Command {
     channel,
     user
   }: CommandContext): Promise<BotResponse> => {
-    const isEditor = await hb.api.seventv.isEditor(user.username!, channel)
+    if (!emote || !alias)
+      return { response: 'emote and alias are required', success: false }
 
-    if (isEditor instanceof ResourceError) {
-      return { response: 'could not fetch editors', success: false }
-    }
+    const isEditor = await this.methods.isEditor(user.username!, channel)
 
-    if (!isEditor.data)
-      return {
-        response: 'You are not an editor of this channel :\\',
-        success: false
-      }
+    if (isEditor instanceof ResourceError)
+      return { response: isEditor.error, success: false }
 
     const emoteData = await hb.api.seventv.rest.getEmoteIdAndName(
       emote,
       channel
     )
 
-    if (emoteData instanceof ResourceError) {
+    if (emoteData instanceof ResourceError)
       return { response: emoteData.error, success: false }
-    }
 
-    const { id: emoteId } = emoteData.data
+    const result = await hb.api.seventv.gql.setAlias(
+      emoteData.data.id,
+      alias,
+      channel
+    )
 
-    const result = await hb.api.seventv.gql.setAlias(emoteId, alias, channel)
-
-    if (result instanceof ResourceError) {
+    if (result instanceof ResourceError)
       return { response: result.error, success: false }
-    }
 
     return {
       response: `Successfully set ${alias} as alias for ${emote}`,
       success: true
+    }
+  }
+
+  methods = {
+    isEditor: async (
+      user: string,
+      channel: string
+    ): Promise<Resource<boolean>> => {
+      const isEditor = await hb.api.seventv.isEditor(user, channel)
+
+      if (isEditor instanceof ResourceError)
+        return new ResourceError('could not fetch editors')
+
+      if (!isEditor.data)
+        return new ResourceError('You are not an editor of this channel :\\')
+
+      return new ResourceSuccess(true)
     }
   }
 }
