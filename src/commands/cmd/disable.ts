2b16044ead96f2c510f9834cfaaf4639eb67958b
helltf <dario.zachow5530@gmail.com>
2022-08-23 10:23:21 +0200

    feat(disable): creating command to disable chat messages

---
+++ src/commands/cmd/disable.ts
@@ -0,0 +1,38 @@
+import { BotResponse } from "@src/client/types";
+import { Command, CommandContext, CommandFlag } from "@src/commands/types";
+import {
+  ChatPermissionLevel,
+  GlobalPermissionLevel
+} from '@src/utilities/permission/types'
+
+export class DisableCommand implements Command {
+  name = 'disable'
+  permissions = GlobalPermissionLevel.USER
+  description = 'disables messages from the bot'
+  requiredParams = []
+  optionalParams = []
+  alias = ['mute']
+  flags = []
+  cooldown = 30000
+  execute = async ({ channel }: CommandContext): Promise<BotResponse> => {
+    const success = await this.methods.disable(channel)
+
+    if (!success) return { response: 'Bot is already disabled', success: false }
+
+    return { response: 'disabled', success: true }
+  }
+  methods = {
+    disable: async (channel: string): Promise<boolean> => {
+      const isDisabled = await hb.db.channel.findOneBy({ channel })
+      console.log(isDisabled)
+      if (isDisabled?.allowed) return false
+
+      const updateResult = await hb.db.channel.update(
+        { channel },
+        { allowed: false }
+      )
+
+      return updateResult.affected !== 0
+    }
+  }
+}
