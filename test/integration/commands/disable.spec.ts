33a0edc3633a168d7dbf071c7d40972cfe146397
helltf <dario.zachow5530@gmail.com>
2022-08-23 10:22:44 +0200

    test(disable): adding tests for execute function

---
+++ test/integration/commands/disable.spec.ts
@@ -0,0 +1,76 @@
+import { DisableCommand } from '@src/commands/cmd/disable'
+import { clearDb } from '@test-utils/clear'
+import { disconnectDatabase } from '@test-utils/disconnect'
+import {
+  getExampleChannel,
+  getExampleTwitchUserState
+} from '@test-utils/example'
+import { setupDatabase } from '@test-utils/setup-db'
+
+describe('disable', () => {
+  let disable: DisableCommand
+
+  beforeAll(async () => {
+    await setupDatabase()
+  })
+
+  beforeEach(async () => {
+    await clearDb()
+    disable = new DisableCommand()
+  })
+
+  afterAll(async () => {
+    await disconnectDatabase()
+  })
+
+  describe('execute', () => {
+    const user = getExampleTwitchUserState({})
+    const channel = 'channel'
+
+    it('channel is already disabled return no success', async () => {
+      jest.spyOn(disable.methods, 'disable').mockResolvedValue(false)
+      const { response, success } = await disable.execute({
+        message: [],
+        user,
+        channel
+      })
+
+      expect(response).toBe('Bot is already disabled')
+      expect(success).toBe(false)
+    })
+
+    it('bot is not disabled return success', async () => {
+      jest.spyOn(disable.methods, 'disable').mockResolvedValue(true)
+      const { response, success } = await disable.execute({
+        message: [],
+        user,
+        channel
+      })
+
+      expect(response).toBe('disabled')
+      expect(success).toBe(true)
+    })
+  })
+
+  describe('disable method', () => {
+    const channel = 'channel'
+
+    it.only('channel is already disabled return false', async () => {
+      const savedChannel = getExampleChannel({ channel, allowed: false })
+      await hb.db.channel.save(savedChannel)
+
+      const success = await disable.methods.disable(channel)
+
+      expect(success).toBe(false)
+    })
+
+    it('channel is allowed return true', async () => {
+      const savedChannel = getExampleChannel({ channel, allowed: true })
+      await hb.db.channel.save(savedChannel)
+
+      const success = await disable.methods.disable(channel)
+
+      expect(success).toBe(true)
+    })
+  })
+})
