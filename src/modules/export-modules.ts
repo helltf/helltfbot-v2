ed03a34ca7c8a72790e5ddb45df9b09aa59b7fd2
helltf <dario.zachow5530@gmail.com>
2022-11-10 20:02:07 +0100

    feat: enable reminder module

--- src/modules/export-modules.ts
+++ src/modules/export-modules.ts
@@ -3,10 +3,12 @@ import { GameModule } from "@modules/game.module";
 import { TimeoutTracking } from "@modules/timeout.module";
 import { Module } from '@modules/types'
 import { ColorTracking } from '@modules/user.module'
+import { ReminderModule } from './reminder.module'
 
 export const modules: Module[] = [
   new TimeoutTracking(),
   new ColorTracking(),
   new BanTracking(),
-  new GameModule()
+  new GameModule(),
+  new ReminderModule()
 ]
