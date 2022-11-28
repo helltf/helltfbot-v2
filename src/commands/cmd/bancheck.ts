72f03f57a15e0a82008ea39b613a6155c304617e
helltf <dario.zachow5530@gmail.com>
2022-11-28 19:17:04 +0100

    feat(bancheck): check for context

--- src/commands/cmd/bancheck.ts
+++ src/commands/cmd/bancheck.ts
@@ -1,24 +1,34 @@
 import { BanEntity } from "@db/entities";
 import { BotResponse } from "@src/client/types";
 import { ChatPermissionLevel, GlobalPermissionLevel } from "@src/utilities/permission/types";
-import { Command, CommandContext, CommandFlag } from '../types'
+import { Command, CommandContext, CommandFlag, MessageType } from '../types'
 
 export class BanCheckCommand implements Command {
   name = 'bancheck'
   permissions = ChatPermissionLevel.USER
   description = 'check bans for a user in a channel'
   requiredParams = []
-  optionalParams = ['user', 'channel']
+  optionalParams = ['channel', 'user']
   alias = ['bc', 'banc']
   flags = [CommandFlag.WHISPER, CommandFlag.LOWERCASE]
   cooldown = 15000
-  execute = async (context: CommandContext): Promise<BotResponse> => {
+  execute = async ({
+    type,
+    channel,
+    message: [givenChannel, givenUser]
+  }: CommandContext): Promise<BotResponse> => {
+    if (type === MessageType.WHISPER && !givenChannel)
+      return {
+        response: 'Channel is required in whisper context',
+        success: false
+      }
+
     return {
-      response: 'Channel is required in whisper context',
+      response: '',
       success: false
     }
   }
-  method = {
+  methods = {
     getBans: async (user: string, channel: string): Promise<BanEntity[]> => {
       return hb.db.ban.findBy({
         user,
