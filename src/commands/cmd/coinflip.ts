7e9c89da53bbc64874c00ebb162bda5bbe0eee7d
helltf <dario.zachow5530@gmail.com>
2022-12-16 23:08:17 +0100

    feat(coinflip): return result of coinflip

--- src/commands/cmd/coinflip.ts
+++ src/commands/cmd/coinflip.ts
@@ -1,17 +1,27 @@
-import { BotResponse } from "@src/client/types";
-import { ChatPermissionLevel, GlobalPermissionLevel } from "@src/utilities/permission/types";
+import { BotResponse } from '@src/client/types'
+import { ChatPermissionLevel } from '@src/utilities/permission/types'
 import { Command, CommandContext, CommandFlag } from '../types'
 
 export class CoinflipCommand implements Command {
-  name: string
-  permissions: ChatPermissionLevel | GlobalPermissionLevel
-  description: string
-  requiredParams: string[]
-  optionalParams: string[]
-  alias: string[]
-  flags: CommandFlag[]
-  cooldown: number
-  methods?: { [key: string]: (...args: any) => any } | undefined
-  execute: (context: CommandContext) => Promise<BotResponse>
-  static?: { [key: string]: any } | undefined
+  name = 'coinflip'
+  permissions = ChatPermissionLevel.USER
+  description = 'flips a coin'
+  requiredParams = []
+  optionalParams = []
+  alias = ['cf']
+  flags = [CommandFlag.WHISPER]
+  cooldown = 15000
+  execute = async (): Promise<BotResponse> => {
+    const flipResult = this.methods.flipCoin()
+
+    return {
+      response: `You flipped ${flipResult ? 'heads' : 'tails'} (${
+        flipResult ? 'yes' : 'no'
+      })`,
+      success: true
+    }
+  }
+  methods = {
+    flipCoin: async (): boolean => {}
+  }
 }
