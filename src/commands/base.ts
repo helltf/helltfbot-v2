0d132d50f5bd76184898576b805ce1bb2b44cb13
helltf <dario.zachow5530@gmail.com>
2022-12-17 15:37:16 +0100

    feat: add abstract basecommand

---
+++ src/commands/base.ts
@@ -0,0 +1,20 @@
+import { BotResponse } from "@src/client/types"
+import { ChatPermissionLevel, GlobalPermissionLevel } from "@src/utilities/permission/types"
+import { Command, CommandContext, CommandFlag } from './types'
+
+export abstract class BaseCommand implements Command {
+  name: string
+  permissions: ChatPermissionLevel | GlobalPermissionLevel
+  description: string
+  requiredParams: string[]
+  optionalParams: string[]
+  alias: string[]
+  flags: CommandFlag[]
+  cooldown: number
+  methods?: { [key: string]: (...args: any) => any } | undefined
+  execute: (context: CommandContext) => Promise<BotResponse>
+  static?: { [key: string]: any } | undefined
+  evaluate = () => {
+    console.log(this.name)
+  }
+}
