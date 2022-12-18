a616ed456f9db10ae561393918c11b433b9e1b3a
helltf <dario.zachow5530@gmail.com>
2022-12-18 18:27:32 +0100

    feat: add new commandparams type

--- src/commands/types.ts
+++ src/commands/types.ts
@@ -1,5 +1,6 @@
 import { ChatPermissionLevel, GlobalPermissionLevel } from "@src/utilities/permission/types"
 import { BotResponse, TwitchUserState } from "../client/types"
+import { BaseCommand } from "./base"
 
 
 export interface Command {
@@ -14,16 +15,27 @@ export interface Command {
   methods?: {
     [key: string]: (...args: any) => Promise<any> | any
   }
-  execute: (context: CommandContext) => Promise<BotResponse>
+  execute<T extends BaseCommand>(
+    context: CommandContext<T>
+  ): Promise<BotResponse>
   static?: {
     [key: string]: any
   }
 }
 
-export interface CommandContext {
+export type CommandParams<T extends BaseCommand> = {
+  [key in
+    | T['optionalParams'][number]
+    | T['requiredParams'][number]]: key extends T['requiredParams'][number]
+    ? string
+    : string | undefined
+}
+
+export interface CommandContext<T extends BaseCommand> {
   user: TwitchUserState
   channel: string
   message: string[]
+  params: CommandParams<T>
   type?: MessageType
 }
 
